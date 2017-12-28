ced6de7311272f446c6e8c058166a3e15bc004f3
i_kiselyov <igorkiselev93@gmail.com>
2017-12-28 23:37:27 +0300

    Правка в jd01_04 TaskC

--- src/by/it/ikiselev/jd01_04/TaskC.java
+++ src/by/it/ikiselev/jd01_04/TaskC.java
@@ -3,58 +3,67 @@ package by.it.ikiselev.jd01_04;
 import by.it.ikiselev.jd01_03.InOut;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TaskC {
-    public static void main(String[] args) {
-       // Scanner sc=new Scanner(System.in);
-       // String line=sc.nextLine();
-       // buildOneDimArray(line);
-        double[] sdr=new double[4];
-        sdr[0]=120;
-        sdr[1]=76;
-        sdr[2]=119;
-        sdr[2]=84;
-        double[] sdr1=mergeSort(sdr);
-        System.out.println(sdr1[0]+" "+sdr1[1]+" "+sdr1[2]+" "+sdr1[3]+" ");
-        System.out.println(binarySearch(sdr1,84));
 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String line=sc.nextLine();
+        //  double[] arr;
+        buildOneDimArray(line);
+        //    double[] d=new double[]{78,15,-8.34,59};
+        //    mergeSort(d);
     }
+
     static int binarySearch(double[] array, double value) {
         int i = -1;
-            int low = 0, high = array.length, mid;
-            while (low < high) {
-                mid = (low + high)/2; // Можно заменить на: (low + high) >>> 1, чтоб не возникло переполнение целого
-                if (value == array[mid]) {
-                    i = mid;
-                    break;
+        int low = 0, high = array.length, mid;
+        while (low < high) {
+            mid = (low + high)/2; // Можно заменить на: (low + high) >>> 1, чтоб не возникло переполнение целого
+            if (value == array[mid]) {
+                i = mid;
+                break;
+            } else {
+                if (value < array[mid]) {
+                    high = mid;
                 } else {
-                    if (value < array[mid]) {
-                        high = mid;
-                    } else {
-                        low = mid + 1;
-                    }
+                    low = mid + 1;
                 }
             }
+        }
         return i;
     }
     static void buildOneDimArray(String line){
-        double[] arr= InOut.getArray(line);
-        InOut.printArray(arr, "V",5);
-        double first=arr[0];
-        double last=arr[arr.length-1];
+        double[] arr=InOut.getArray(line);
+        // InOut.printArray(arr, "V",5);
+        //  double first=arr[0];
+        //  double last=arr[arr.length-1];
+        //  double[] copy=arr;
         mergeSort(arr);
-        InOut.printArray(arr,"V",4);
-        int indexFirst= binarySearch(arr,first);
-        int indexLast=binarySearch(arr, last);
-        System.out.printf("Index of first element=%d\n",binarySearch(arr,first));
-        System.out.printf("Index of last element=%d\n",binarySearch(arr,last));
+        System.out.println(arr[0]);
+        //  copy=mergeSort1(arr);
+        //  InOut.printArray(copy,"V",4);
+        //  int indexFirst= binarySearch(copy,first);
+        //  int indexLast=binarySearch(copy, last);
+        //  System.out.printf("Index of first element=%d\n",indexFirst);
+        //  System.out.printf("Index of last element=%d\n",indexLast);
     }
-    static double[] mergeSort(double[] arr) {
+    static void mergeSort(double[] array){
+        double first=array[0];
+        double last=array[array.length-1];
+        array=mergeSort1(array);
+        InOut.printArray(array,"V",4);
+        int indexFirst= binarySearch(array,first);
+        int indexLast=binarySearch(array, last);
+        System.out.printf("Index of first element=%d\n",indexFirst);
+        System.out.printf("Index of last element=%d\n",indexLast);
+    }
+    static double[] mergeSort1(double[] arr) {
         int len = arr.length;
         if (len < 2) return arr;
-     //   System.out.println("hf1");
         int middle = len / 2;
-        return merge(mergeSort(Arrays.copyOfRange(arr, 0, middle)),mergeSort(Arrays.copyOfRange(arr, middle, len)));
+        return merge(mergeSort1(Arrays.copyOfRange(arr, 0, middle)), mergeSort1(Arrays.copyOfRange(arr, middle, len)));
     }
     static double[] merge(double[] arr_1, double[] arr_2) {
         int len_1 = arr_1.length, len_2 = arr_2.length;
@@ -70,7 +79,6 @@ public class TaskC {
                 result[i] = arr_1[a++];
             }
         }
-     //   System.out.println("hf2");
         return result;
     }
 }
