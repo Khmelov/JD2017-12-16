4ebe8c125f3c8f392e51d26dc34fdb8e6b06fbb5
George <georgekrasutski@gmail.com>
2017-12-28 15:06:50 +0300

    TaskA2 and TaskA3 complete

--- src/by/it/krasutski/jd01_05/Main.java
+++ src/by/it/krasutski/jd01_05/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         taskA1();
+        taskA2();
+        taskA3();
     }
 
     static void taskA1() {
@@ -17,4 +19,21 @@ public class Main {
         System.out.println("Result z = "+z);
     }
 
+    static void taskA2(){
+        double a=1.21;
+        double b=0.371;
+        double y=tan((pow(a+b,2)));
+        y=y-cbrt(a+1.5);
+        y=y+a*pow(b,5);
+        y=y-(b/log(abs(pow(a,2))));
+        System.out.println("Result y = "+y);
+    }
+
+    static void taskA3(){
+        double x=12.1;
+        for (double i = -5; -5<=i&&i<=12; i=i+3.75) {
+            double f=exp(i*x)-3.45*i;
+            System.out.println("При a= "+i+" f="+f);
+        }
+    }
 }
