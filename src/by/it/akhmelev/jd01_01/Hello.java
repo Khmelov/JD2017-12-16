d7979ed4fe6b07c0097c1320e2b72a294a892657
i.sukach <ihar.sukach@gmail.com>
2017-12-28 12:37:59 +0300

    TaskA complete

---
+++ src/by/it/akhmelev/jd01_01/Hello.java
@@ -0,0 +1,14 @@
+package by.it.akhmelev.jd01_01;
+
+public class Hello {
+
+    private String slogan="Hello world";
+
+    void printSlogan(){
+        System.out.println(slogan);
+    }
+
+    public void setSlogan(String slogan) {
+        this.slogan = slogan;
+    }
+}
