e2a564a57d47c17c7edc8e0775a9b536ea7810af
Pavel <pavel3423@gmail.com>
2017-12-28 12:38:01 +0300

    add jd_01 jd_02 jd_03 jd_04

--- src/by/it/kozlov/jd01_04/Test_jd01_04.java
+++ src/by/it/kozlov/jd01_04/Test_jd01_04.java
@@ -59,7 +59,7 @@ public class Test_jd01_04 {
         ok.include("V[").include("]=0").include("]=123");
         ok.include("first element=10");
         ok.include("last element=2");
-        System.out.println("Проверка метода main завершена");
+        System.out.println("Проверка метода Main завершена");
     }
 
     @Test(timeout = 1500)
@@ -264,7 +264,7 @@ public class Test_jd01_04 {
         if (runMain) //если нужно запускать, то запустим, иначе оставим только вывод
             try {
                 Class[] argTypes = new Class[]{String[].class};
-                Method main = aClass.getDeclaredMethod("main", argTypes);
+                Method main = aClass.getDeclaredMethod("Main", argTypes);
                 main.invoke(null, (Object) new String[]{});
                 System.setOut(oldOut); //возврат вывода, нужен, только если был запуск
             } catch (Exception x) {
