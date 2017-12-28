81f9a151855271911b43b223c5376d22484d4457
i_kiselyov <igorkiselev93@gmail.com>
2017-12-28 23:33:52 +0300

    Task B4,B5 complete

--- src/by/it/ikiselev/jd01_05/Main.java
+++ src/by/it/ikiselev/jd01_05/Main.java
@@ -22,12 +22,38 @@ public class Main {
             double y=pow(E,a*x)-3.45*a;
             System.out.printf("При а=%f\ty=%f",a,y);
             System.out.println();
-
+        }
+    }
+    static void taskB4(){
+        for (double a = 0; a <=2 ; a=a+0.2) {
+            double y=0;
+            for (int x = 1; x < 7; x++) {
+                y=y+pow(7.0,a)-cos(x);
+            }
+            System.out.printf("При а=%f\ty=%f",a,y);
+            System.out.println();
+        }
+    }
+    static void taskB5(){
+        double b=0;
+        for (double x = -6; x < 2; x=x+0.5) {
+            if (x/2>-2 && x/2<=-1){
+                b=sin(x*x);
+            }
+            if (x/2>-1 && x/2<=0.2){
+                b=cos(x*x);
+            }
+            if (x/2==0.2) {
+                b = 1 / tan(x * x);
+            }
+            System.out.printf("При x=%f\ty=%f",x,log(abs(b+2.74)));
+            System.out.println();
         }
     }
     public static void main(String[] args) {
         taskA1();
         taskA2();
         taskA3();
+        taskB4();
     }
 }
