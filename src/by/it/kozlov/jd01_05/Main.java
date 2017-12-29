f929cd29b91f47652847f2bd73d1e4edcd30445a
Pavel <pavel3423@gmail.com>
2017-12-29 23:34:38 +0300

    add jd_05

--- src/by/it/kozlov/jd01_05/Main.java
+++ src/by/it/kozlov/jd01_05/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void Main(String[] args) {
         taskA1();
+        taskA2(1.21, 0.371);
+        taskA3(12.1, 3.75);
     }
 
     static void taskA1() {
@@ -14,7 +16,20 @@ public class Main {
         double z = cos((pow(x * x + PI / 6, 5)));
         z = z - sqrt(x * pow(a, 3));
         z = z - log(abs((a - 1.12 * x) / 4));
-        System.out.println("Result z=" + z);
+        System.out.println("Result z = " + z);
     }
 
+    static void taskA2(double a, double b) {
+        double y;
+        y = tan(pow((a + b), 2.0)) - pow((a + 1.5), (1.0 / 3.0)) + a * pow(b, 5.0) - b / log(a * a);
+        System.out.println("Result y = " +y);
+    }
+
+    static void taskA3(double x, double a) {
+        double f;
+        for (double i = -5; i <= 12; i+=3.75) {
+            f=pow(exp(-3.54*a),(a*x));
+            System.out.printf("При a = %.2f f = %f\n", i, f);
+        }
+    }
 }
