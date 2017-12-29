ba2fdb1a3909f4fb905ea880818ad22aadc9f2ab
VladSVBY <vladsvby@yandex.by>
2017-12-29 15:33:57 +0300

    Task 1,2,3,4,5,6 were done

--- src/by/it/sevashko/jd01_05/Task4.java
+++ src/by/it/sevashko/jd01_05/Task4.java
@@ -2,15 +2,11 @@ package by.it.sevashko.jd01_05;
 
 public class Task4 {
 
-    static double calculate(double n){
+    static void calculate(double x){
         double y;
         for (double a = 0; a < 2; a+=0.2) {
-            y = 0;
-            for (int x = 1; x < 7; x++) {
-                y = y + Math.pow(7, a) - Math.cos(x);
-            }
-            System.out.println(y);
+            y = 6 * Math.pow(7, a) - Math.cos(x);
+            System.out.printf("При a=%.2f Сумма y = %e\n", a, y);
         }
-        return n;
     }
 }
