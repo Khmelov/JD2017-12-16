f18c0e6455118b32f8c57c378d0d77aaeaeb3f6e
NoOne4 <wezman1main@mail.ru>
2017-12-30 11:06:33 +0300

    complete 01 -05

--- src/by/it/korobeinikov/jd01_05/Main.java
+++ src/by/it/korobeinikov/jd01_05/Main.java
@@ -1,15 +1,13 @@
 package by.it.korobeinikov.jd01_05;
-
 import static java.lang.Math.*;
 
 public class Main {
     static void taskA1() {
         double a=756.13;
         double x=0.3;
-        double z= cos((pow(x*x+PI/6,5)));
-        z=z-sqrt(x*pow(a,3));
-        z=z-log(abs((a-1.12*x)/4));
-        System.out.println(z);}
+        double z=cos(pow((x*x+PI/6),5))-sqrt(x*pow(a,3))-log10(abs((a-1.21*x)/4));
+        System.out.println(z);
+    }
     public static void main(String[] args) {
         taskA1();
     }
