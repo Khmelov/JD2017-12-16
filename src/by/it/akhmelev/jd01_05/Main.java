47fa8717b6141a4c3ec7fcc6f8978cd89c702d24
i.sukach <ihar.sukach@gmail.com>
2017-12-30 11:06:22 +0300

    Sample classwork 01...05

--- src/by/it/akhmelev/jd01_05/Main.java
+++ src/by/it/akhmelev/jd01_05/Main.java
@@ -4,12 +4,12 @@ import static java.lang.Math.*;
 
 public class Main {
     static void taskA1(){
-        double a=756.13;
-        double x=0.3;
-        double z=cos((pow(x*x+PI/6,5)));
-        z=z-sqrt(x*pow(a,3));
-        z=z-log(abs((a-1.12*x)/4));
-        System.out.println("Result z="+z);
+         double a=756.13;
+         double x=0.3;
+         double z=cos((pow(x*x+PI/6,5)));
+         z=z-sqrt(x*pow(a,3));
+         z=z-log(abs((a-1.12*x)/4));
+         System.out.println("Result z="+z);
     }
 
 
