1c9df4ad9aa3cf30b3ad0e6cfd350bddb8b0a35b
i.sukach <ihar.sukach@gmail.com>
2017-12-30 12:14:59 +0300

    Sample classwork A1 A2 06

---
+++ src/by/it/akhmelev/jd01_06/classwork/jd01_06/TaskA1.java
@@ -0,0 +1,22 @@
+package by.it.akhmelev.jd01_06.classwork.jd01_06;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskA1 {
+    public static void main(String[] args) {
+        Pattern pattern=Pattern.compile("[а-яА-ЯёЁ]{4,}");
+        Matcher matcher=pattern.matcher(Poem.text);
+        StringBuilder sb=new StringBuilder(Poem.text);
+        while (matcher.find()){
+            int pos=matcher.start()+3;
+            sb.replace(pos,pos+1,"#");
+            int len=matcher.end()-matcher.start();
+            if (len>6) {
+                pos=matcher.start()+6;
+                sb.replace(pos,pos+1,"#");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
