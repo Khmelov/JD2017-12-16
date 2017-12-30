ba0ecdebc2411e0880e630159aede7b74e6606fc
George <georgekrasutski@gmail.com>
2017-12-30 12:10:30 +0300

    TaskA1 and TaskA2 complete.

--- src/by/it/krasutski/jd01_03/Helper.java
+++ src/by/it/krasutski/jd01_03/Helper.java
@@ -4,17 +4,16 @@ import java.util.Arrays;
 
 public class Helper {
     /**
-     *
      * @param arr входной массив
      * @return возвращает минимум
      */
-    static double findMin(double[ ] arr){
-        if (0==arr.length){
+    static double findMin(double[] arr) {
+        if (0 == arr.length) {
             return -Double.MAX_VALUE;
         } else {
             double min = arr[0];
             for (double m : arr) {
-                if (min>m) min=m;
+                if (min > m) min = m;
             }
             System.out.println(min);
             System.out.println();
@@ -23,17 +22,16 @@ public class Helper {
     }
 
     /**
-     *
      * @param arr входной массив
      * @return возвращает максимум
      */
-    static double findMax(double[ ] arr) {
+    static double findMax(double[] arr) {
         if (0 == arr.length) {
             return Double.MAX_VALUE;
         } else {
             double max = arr[0];
             for (double m : arr) {
-                if (max<m) max = m;
+                if (max < m) max = m;
             }
             System.out.println(max);
             System.out.println();
@@ -42,19 +40,18 @@ public class Helper {
     }
 
     /**
-     *
      * @param arr входной массив
      */
-    static void sort(double[ ] arr){
+    static void sort(double[] arr) {
         boolean swap;
-        double last=arr.length-1;
-        do{
-            swap=false;
+        double last = arr.length - 1;
+        do {
+            swap = false;
             for (int i = 0; i < last; i++) {
-                if (arr[i]>arr[i+1]){
+                if (arr[i] > arr[i + 1]) {
                     double buffer = arr[i];
-                    arr[i] = arr[i+1];
-                    arr[i+1] = buffer;
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = buffer;
                     swap = true;
                 }
             }
@@ -65,22 +62,20 @@ public class Helper {
     }
 
     /**
-     *
      * @param arr входной массив
      */
-    static void printMas(double[] arr){
+    static void printMas(double[] arr) {
         for (double m : arr) System.out.print(m + " ");
         System.out.println();
     }
 
     /**
-     *
      * @param matrix входная матрица
      * @param vector входной вектор
      * @return возвращет результат
      */
-    public static double[ ] mul(double[ ][ ] matrix, double[ ] vector){
-        double[] result=new double[matrix.length];
+    public static double[] mul(double[][] matrix, double[] vector) {
+        double[] result = new double[matrix.length];
         for (int i = 0; i < matrix.length; i++)
             for (int j = 0; j < vector.length; j++) {
                 result[i] = result[i] + matrix[i][j] * vector[j];
@@ -92,17 +87,16 @@ public class Helper {
     }
 
     /**
-     *
-     * @param matrixLeft входная матрица
+     * @param matrixLeft  входная матрица
      * @param matrixRight входная матрица
      * @return возвращает результат
      */
-    static double[ ][ ] mul(double[ ][ ] matrixLeft, double[ ][ ] matrixRight){
-        double[][] result=new double[matrixLeft.length][matrixRight[0].length];
+    static double[][] mul(double[][] matrixLeft, double[][] matrixRight) {
+        double[][] result = new double[matrixLeft.length][matrixRight[0].length];
         for (int i = 0; i < matrixLeft.length; i++)
             for (int j = 0; j < matrixRight[0].length; j++)
                 for (int k = 0; k < matrixRight.length; k++) {
-                    result[i][j]=result[i][j] + matrixLeft[i][k]*matrixRight[k][j];
+                    result[i][j] = result[i][j] + matrixLeft[i][k] * matrixRight[k][j];
                 }
         printMatrix(result);
         System.out.println();
@@ -110,19 +104,17 @@ public class Helper {
     }
 
     /**
-     *
      * @param vector входной массив
      */
-    static void printVector(double[] vector){
+    static void printVector(double[] vector) {
         for (double v : vector) System.out.print(v + " ");
         System.out.println();
     }
 
     /**
-     *
      * @param matrixA входной двумерный массив
      */
-    static void printMatrix(double[][] matrixA){
+    static void printMatrix(double[][] matrixA) {
         for (int i = 0; i < matrixA.length; i++) {
             System.out.println(Arrays.toString(matrixA[i]));
         }
--- src/by/it/krasutski/jd01_04/TaskA.java
+++ src/by/it/krasutski/jd01_04/TaskA.java
@@ -6,12 +6,12 @@ public class TaskA {
 
     public static void main(String[] args) {
         printMulTable();
-        Scanner sc=new Scanner(System.in);
-        String line=sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
         buildOneDimArray(line);
     }
-    
-    private static void printMulTable(){
+
+    private static void printMulTable() {
         for (int i = 2; i < 10; i++) {
             for (int j = 2; j < 10; j++) {
                 System.out.printf("%1d*%1d=%-2d ", i, j, i * j);
@@ -21,23 +21,23 @@ public class TaskA {
 
     }
 
-    private static int find(double[] arr, double value){
+    private static int find(double[] arr, double value) {
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i]==value)
+            if (arr[i] == value)
                 return i;
         }
         return -1;
     }
 
-    private static void buildOneDimArray(String line){
-        double[] arr=InOut.getArray(line);
-        InOut.printArray(arr,"V",5);
-        double first=arr[0];
-        double last=arr[arr.length-1];
+     static void buildOneDimArray(String line) {
+        double[] arr = InOut.getArray(line);
+        InOut.printArray(arr, "V", 5);
+        double first = arr[0];
+        double last = arr[arr.length - 1];
         Helper.sort(arr);
-        InOut.printArray(arr,"V",4);
-        System.out.printf("Index of first element=%d\n",find(arr,first));
-        System.out.printf("Index of last element=%d\n",find(arr,last));
+        InOut.printArray(arr, "V", 4);
+        System.out.printf("Index of first element=%d\n", find(arr, first));
+        System.out.printf("Index of last element=%d\n", find(arr, last));
     }
 
 }
