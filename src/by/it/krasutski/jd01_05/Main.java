c4f9ce9e4205b46bff6e5eb17cc8c4142aa45a16
George <georgekrasutski@gmail.com>
2017-12-30 09:24:03 +0300

    TaskC1 complete

--- src/by/it/krasutski/jd01_05/Main.java
+++ src/by/it/krasutski/jd01_05/Main.java
@@ -1,5 +1,7 @@
 package by.it.krasutski.jd01_05;
 
+import java.util.Random;
+
 import static java.lang.Math.*;
 
 public class Main {
@@ -10,6 +12,7 @@ public class Main {
         taskA3();
         taskB1();
         taskB2();
+        taskC1();
     }
 
     static void taskA1() {
@@ -19,6 +22,7 @@ public class Main {
         z=z-sqrt(x*pow(a,3));
         z=z-log(abs((a-1.12*x)/4));
         System.out.println("Result z = "+z);
+        System.out.println();
     }
 
     static void taskA2(){
@@ -29,6 +33,7 @@ public class Main {
         y=y+a*pow(b,5);
         y=y-(b/log(abs(pow(a,2))));
         System.out.println("Result y = "+y);
+        System.out.println();
     }
 
     static void taskA3(){
@@ -47,6 +52,7 @@ public class Main {
             }
             System.out.println("При a= "+a+" y="+y);
         }
+        System.out.println();
     }
 
     static void taskB2(){
@@ -67,6 +73,46 @@ public class Main {
             else
                 System.out.println("При x/2="+x/2+" вычисления не определены");
         }
+        System.out.println();
+    }
+
+    static void taskC1(){
+        int v=5;
+        Random random=new Random();
+        int n=20+random.nextInt(41-20);
+        double x= 3.75/(n+1);
+        double[] arra=new double[n];
+        for (int i = 0; i < arra.length; i++) {
+            for (double j = 5.33+x; 5.33<j&&j<9; j=j+x) {
+                double z=cbrt(j*j+4.5);
+                arra[i++]=z;
+                System.out.printf("A[% -3d] = "+"%2.4f"+" ",i-1,arra[i-1]);
+                if ((0==(i)%v) || (arra.length-1==i-1)){
+                    System.out.println();
+                }
+            }
+        }
+        System.out.println();
+        double[] arrb=new double[arra.length];
+        double key=3.5;
+        int k=0;
+        for (int i = 0; i < arra.length; i++) {
+            if (arra[i]>key){
+                arrb[k++]=arra[i];
+                System.out.printf("B[% -3d] = "+"%2.4f"+" ",k-1,arrb[k-1]);
+                if ((0==(k)%v) || (arrb.length-1==k)){
+                    System.out.println();
+                }
+            }
+        }
+        System.out.println();
+        System.out.println();
+        double average=1;
+        for (int j = 0; j < k; j++) {
+            average=average*arrb[j];
+        }
+        average=pow(average,1./arrb.length);
+        System.out.printf("Среднее геометрическое массива B = %2.4f",average);
     }
 
 }
