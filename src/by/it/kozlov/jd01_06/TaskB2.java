5c9d4b15107abc83b751908e9ee2754699f85fdc
Pavel <pavel3423@gmail.com>
2018-01-02 18:30:24 +0300

    complite jd_06 TaskB2(delete char)

--- src/by/it/kozlov/jd01_06/TaskB2.java
+++ src/by/it/kozlov/jd01_06/TaskB2.java
@@ -6,18 +6,21 @@ import java.util.regex.Pattern;
 public class TaskB2 {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder(Poem.text);
-        sb = deleteSign(sb);
+        //sb = deleteSign(sb);
         sb = deleteElipsis(sb);
-        sb.trimToSize();
+
 
         System.out.println(sb.toString());
     }
 
     private static StringBuilder deleteElipsis(StringBuilder sb) {
-        Pattern pattern = Pattern.compile("\\.{3}");
+        Pattern pattern = Pattern.compile("\\.{3}|-|,|:|!");
         Matcher matcher = pattern.matcher(Poem.text);
+        int coefficient = 0;
         while (matcher.find()) {
-            sb.replace(matcher.start(), matcher.end(), " ");
+            int start = matcher.start() - coefficient;
+            sb.replace(start, start+matcher.end()-matcher.start(), "");
+            coefficient=coefficient+matcher.end()-matcher.start();
         }
         return sb;
     }
