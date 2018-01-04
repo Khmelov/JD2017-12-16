8cc3e408429185086f835d629e738f21d821e34f
IrinaSendetskaya <Irene_siv@inbox.ru>
2018-01-04 23:14:26 +0300

    Tasks 07 complete

---
+++ src/by/it/sendetskaya/jd01_07/Runner.java
@@ -0,0 +1,32 @@
+package by.it.sendetskaya.jd01_07;
+
+public class Runner {
+    public static void main(String[] args) {
+        Scalar scalar1=new Scalar(3.1233343);
+        System.out.println("scalar1="+scalar1);
+
+        Scalar scalar2=new Scalar(scalar1);
+        System.out.println("scalar2="+scalar2);
+
+        Scalar scalar3=new Scalar("4.1233343");
+        System.out.println("scalar3="+scalar3);
+
+        Vector vector1=new Vector( new double[]{1,2,3});
+        System.out.println("vector1="+vector1);
+
+        Vector vector2=new Vector( vector1);
+        System.out.println("vector2="+vector2);
+
+        Vector vector3=new Vector( "{1,2,4}");
+        System.out.println("vector3="+vector3);
+
+        Matrix matrix1=new Matrix(new double[][]{{1,2},{3,4}});
+        System.out.println("matrix1="+matrix1);
+
+        Matrix matrix2=new Matrix( matrix1);
+        System.out.println("matrix2="+matrix2);
+
+        Matrix matrix3=new Matrix("{1,2},{3,4}");
+        System.out.println("matrix3="+matrix3);
+    }
+}
