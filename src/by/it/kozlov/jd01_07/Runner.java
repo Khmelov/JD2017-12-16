86019ea78d5d9d321d70d6a46134e99b4c2efc01
Pavel <pavel3423@gmail.com>
2018-01-04 11:52:52 +0300

    complite jd_07

--- src/by/it/kozlov/jd01_07/Runner.java
+++ src/by/it/kozlov/jd01_07/Runner.java
@@ -2,19 +2,13 @@ package by.it.kozlov.jd01_07;
 
 public class Runner {
     public static void main(String[] args) {
-        Scalar scalar1 = new Scalar(3.2154);
-        System.out.println("scalar1=" + scalar1);
+        Scalar scalar1=new Scalar(3.2154);
+        System.out.println("scalar1="+scalar1);
 
-        Scalar scalar2 = new Scalar(scalar1);
-        System.out.println("scalar2=" + scalar2);
+        Scalar scalar2=new Scalar(scalar1);
+        System.out.println("scalar2="+scalar2);
 
-        Scalar scalar3 = new Scalar(4.2154);
-        System.out.println("scalar3=" + scalar3);
-
-        Vector vector1 = new Vector(new double[]{1, 2, 3});
-        System.out.println("vector1=" + vector1);
-
-        Vector vector2 = new Vector(vector1);
-        System.out.println("vector2=" + vector2);
+        Scalar scalar3=new Scalar(4.2154);
+        System.out.println("scalar3="+scalar3);
     }
 }
