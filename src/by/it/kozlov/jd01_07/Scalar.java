eb615bd420c533628fb1918828fa1335f316d398
Pavel <pavel3423@gmail.com>
2018-01-04 11:52:52 +0300

    complite jd_07

--- src/by/it/kozlov/jd01_07/Scalar.java
+++ src/by/it/kozlov/jd01_07/Scalar.java
@@ -1,4 +1,22 @@
 package by.it.kozlov.jd01_07;
 
-public class Scalar {
+class Scalar extends AbstractVar {
+    private double value;
+
+    Scalar(double value) {
+        this.value = value;
+    }
+
+    Scalar(Scalar other) {
+        this.value = other.value;
+    }
+
+    Scalar(String string) {
+        this.value = Double.parseDouble(string);
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString(value);
+    }
 }
