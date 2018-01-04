7993a98142a8198243652daa0e416ac2761c1179
i_kiselyov <igorkiselev93@gmail.com>
2018-01-04 12:08:28 +0300

    Правка в jd01_07 TaskA1,A2,B1,B2

---
+++ src/by/it/ikiselev/jd01_07/Scalar.java
@@ -0,0 +1,18 @@
+package by.it.ikiselev.jd01_07;
+
+class Scalar extends AbstractVar{
+    private double value;
+    public Scalar (double value){
+        this.value=value;
+    }
+    Scalar (Scalar other){
+        this.value=other.value;
+    }
+    Scalar (String string){
+        this.value=Double.parseDouble(string);
+    }
+    @Override
+    public String toString() {
+        return Double.toString(value);
+    }
+}
