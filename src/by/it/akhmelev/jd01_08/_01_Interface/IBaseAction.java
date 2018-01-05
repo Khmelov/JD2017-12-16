afb8c83e87e5b2f5db9a3996767e82e01035e500
Khmelov <375336849110@tut.by>
2018-01-05 21:49:45 +0300

    Add jd01_08 (samples)

---
+++ src/by/it/akhmelev/jd01_08/_01_Interface/IBaseAction.java
@@ -0,0 +1,8 @@
+package by.it.akhmelev.jd01_08._01_Interface;
+
+public interface IBaseAction {
+    boolean openAccount();
+    boolean closeAccount();
+    void blocking();
+    void unBlocking();
+}
