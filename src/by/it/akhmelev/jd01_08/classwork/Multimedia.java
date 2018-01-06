0bb1eed83378af3cb53628ef377f9ea09e648b49
i.sukach <ihar.sukach@gmail.com>
2018-01-06 12:28:34 +0300

    Sample classwork A jd01_08

---
+++ src/by/it/akhmelev/jd01_08/classwork/Multimedia.java
@@ -0,0 +1,12 @@
+package by.it.akhmelev.jd01_08.classwork;
+
+interface Multimedia {
+
+    void powerOn();
+    void powerOff();
+    boolean play(String track);
+    boolean play();
+    boolean stop();
+    boolean pause();
+
+}
