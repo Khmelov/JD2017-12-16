c1b13f8b14f4fae8d5a7b92ad3d04e2d1dfb9f66
Pavel <pavel3423@gmail.com>
2018-01-08 18:14:04 +0300

    complite jd_07 C1

---
+++ src/by/it/kozlov/jd01_08/Building.java
@@ -0,0 +1,11 @@
+package by.it.kozlov.jd01_08;
+
+public interface Building {
+    void windows();
+
+    void door();
+
+    void floor();
+
+    void entrance();
+}
---
+++ src/by/it/kozlov/jd01_08/PublicBuilding.java
@@ -0,0 +1,10 @@
+package by.it.kozlov.jd01_08;
+
+public abstract class PublicBuilding implements Building {
+    private int windows;
+    private int door;
+    private int floor;
+    private int entrance;
+
+
+}
