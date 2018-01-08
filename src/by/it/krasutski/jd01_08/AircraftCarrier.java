f28e756aedfcd840e06f226eb22f15f2f7b6ad2c
George <georgekrasutski@gmail.com>
2018-01-08 23:14:17 +0300

    Improvements.

--- src/by/it/krasutski/jd01_08/AircraftCarrier.java
+++ src/by/it/krasutski/jd01_08/AircraftCarrier.java
@@ -5,7 +5,7 @@ public class AircraftCarrier extends Warship {
     @Override
     public boolean moveTo(String str) {
         setDest(str);
-        boolean result=super.moveTo();
+        boolean result = super.moveTo();
         if (result)
             System.out.println(this);
         return result;
@@ -14,7 +14,7 @@ public class AircraftCarrier extends Warship {
     @Override
     public boolean alarm(String s) {
         setAlarm(s);
-        boolean result=super.alarm();
+        boolean result = super.alarm();
         if (result)
             System.out.println(this);
         return result;
--- src/by/it/krasutski/jd01_08/Ship.java
+++ src/by/it/krasutski/jd01_08/Ship.java
@@ -2,14 +2,23 @@ package by.it.krasutski.jd01_08;
 
 public interface Ship {
     void engineOn();
+
     void engineOff();
+
     boolean moveTo(String str);
+
     boolean moveTo();
+
     boolean anchor();
+
     boolean inPort();
+
     boolean horn();
+
     boolean alarm(String s);
+
     boolean alarm();
+
     boolean alarmOff();
 
 }
