46590fd5a64a6b4f0fad03127423196547185fec
Василий <mobil5662016@gmail.com>
2018-01-09 03:00:34 +0300

    lesson 08 - complete!

---
+++ src/by/it/shalukhin/jd01_06/TaskC1.java
@@ -0,0 +1,41 @@
+package by.it.shalukhin.jd01_06;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskC1 {
+    public static void main(String[] args) {
+        String[] stroki = Poem.text.split("\n");
+        for (String s : stroki) {
+            System.out.println(s);
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < stroki.length; i++) {
+            if (stroki[i].length()>max) max = stroki[i].length();
+        }
+        System.out.println(max);
+
+        for (int i = 0; i < stroki.length; i++) {
+            StringBuilder sb = new StringBuilder(stroki[i]);
+            Pattern pat = Pattern.compile("\\s+");
+            Matcher mat = pat.matcher(sb);
+            int n = max - stroki[i].length();
+            int k = 0;
+            while (n!=0){
+                if (mat.find(k+1)) {
+                    sb.replace(mat.start(),mat.end(), mat.group()+" ");
+                    n--;
+                    k = mat.end();
+                }
+                else{
+                    mat.reset();
+                    k=0;
+                }
+            }
+            stroki[i] = sb.toString();
+        }
+        for (String s : stroki) {
+            System.out.println(s);
+        }
+    }
+}
---
+++ src/by/it/shalukhin/jd01_08/Runner.java
@@ -0,0 +1,39 @@
+package by.it.shalukhin.jd01_08;
+
+public class Runner {
+    public static void main(String[] args) {
+
+
+        LaserPrinter l = new LaserPrinter("HP1", 10, 5);
+
+        System.out.println(l.toString());
+
+        l.powerOn();
+
+        System.out.println(l.toString());
+
+        l.lastPrint();
+
+        l.loadPaper(3);
+
+        System.out.println(l.toString());
+
+        l.printTestPage();
+
+        System.out.println(l.toString());
+
+        l.loadResurs();
+
+        l.powerOff();
+
+        l.loadResurs();
+
+        System.out.println(l.toString());
+
+        l.powerOn();
+
+        System.out.println(l.toString());
+
+
+    }
+}
