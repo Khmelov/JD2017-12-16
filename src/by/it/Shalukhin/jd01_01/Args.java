b01aba824576f0c2daf8ab70ecd2ea9726a08ed7
Василий <mobil5662016@gmail.com>
2018-01-09 03:00:34 +0300

    lesson 08 - complete!

--- src/by/it/Shalukhin/jd01_01/Args.java
+++
@@ -1,9 +0,0 @@
-package by.it.Shalukhin.jd01_01;
-
-public class Args {
-    void printArguments(String[] args){
-        for (String arg : args) {
-            System.out.println("Arg="+arg);
-        }
-    }
-}
