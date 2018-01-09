b2d1952863e7099f02355cce0ad860c5b5a5e91f
i_kiselyov <igorkiselev93@gmail.com>
2018-01-09 12:28:34 +0300

    jd01_09 начало

---
+++ src/by/it/ikiselev/jd01_09/ConsoleRunner.java
@@ -0,0 +1,10 @@
+package by.it.ikiselev.jd01_09;
+
+public class ConsoleRunner {
+    public static void main(String[] args) {
+        Var one=new Scalar(2);
+        Var two=new Scalar(3);
+        Var res=one.add(two);
+        System.out.println(res);
+    }
+}
---
+++ src/by/it/ikiselev/jd01_09/Var.java
@@ -0,0 +1,24 @@
+package by.it.ikiselev.jd01_09;
+
+public abstract class Var implements Operation{
+    @Override
+    public Var add(Var var) {
+        return null;
+    }
+
+    @Override
+    public Var mul(Var var) {
+        return null;
+    }
+
+    @Override
+    public Var div(Var var) {
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+
+        return null;
+    }
+}
