8b19f13714921a9041940cc874fc3db11c705bde
karpeichyk <pashkar@bk.ru>
2018-01-09 19:50:03 +0300

    06 -A , 07-A,B

---
+++ src/by/it/karpeichyk/jd01_08/Multimedia.java
@@ -0,0 +1,18 @@
+package by.it.karpeichyk.jd01_08;
+
+/**
+ * Created by user on 06.01.2018.
+ */
+public interface Multimedia {
+    void powerOn();
+    void  powerOff();
+    boolean  play(String track);
+    boolean stop();
+    boolean pause();
+
+
+
+
+
+
+}
