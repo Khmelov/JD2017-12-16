ee3633f720c0320c433e7e07d46c9d5a4480c7d4
VladSVBY <vladsvby@yandex.by>
2018-01-09 21:06:23 +0300

    jd01_09 ok

---
+++ src/by/it/sevashko/jd01_08/Launcher.java
@@ -0,0 +1,11 @@
+package by.it.sevashko.jd01_08;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        Plane plane1 = new Fighter();
+        //plane1.engineStart();
+        //plane1.engineShutdown();
+        plane1.flyUp();
+    }
+}
