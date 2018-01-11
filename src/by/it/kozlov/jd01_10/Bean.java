27672fe6cbab3f0878164c04fdbc546e9be05ea8
Pavel <pavel3423@gmail.com>
2018-01-11 11:28:05 +0300

    complite jd_10 A1

---
+++ src/by/it/kozlov/jd01_10/Bean.java
@@ -0,0 +1,5 @@
+package by.it.kozlov.jd01_10;
+
+public class Bean {
+
+}
---
+++ src/by/it/kozlov/jd01_10/Param.java
@@ -0,0 +1,14 @@
+package by.it.kozlov.jd01_10;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Param {
+    int a();
+
+    int b();
+}
