b85afa34be34a8526aa25e9490c5ec65e32e9b82
George <georgekrasutski@gmail.com>
2018-01-11 23:10:22 +0300

    TaskB(1,2) and TaskC complete.

---
+++ src/by/it/krasutski/jd01_10/BeanTester.java
@@ -0,0 +1,24 @@
+package by.it.krasutski.jd01_10;
+
+import java.lang.reflect.Method;
+
+public class BeanTester {
+    public static void main(String[] args) {
+        Class structBean = Bean.class;
+        Method[] methods = structBean.getDeclaredMethods();
+        Object obj;
+        try {
+            for (Method method : methods) {
+                if (method.isAnnotationPresent(Param.class)) {
+                    obj = structBean.getConstructor().newInstance();
+                    Param annotation = method.getAnnotation(Param.class);
+                    System.out.println("Метод " + method.getName() + " : "
+                            + method.invoke(obj, annotation.a(), annotation.b()));
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/by/it/krasutski/jd01_10/PrintMath.java
+++ src/by/it/krasutski/jd01_10/PrintMath.java
@@ -1,24 +1,28 @@
 package by.it.krasutski.jd01_10;
 
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 public class PrintMath {
-    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
-        Class<?> structMath = String.class;
+    public static void main(String[] args) {
+        Class<?> structMath = Math.class;
         Method[] methods = structMath.getDeclaredMethods();
         for (Method method : methods) {
-//            if ((method.getModifiers() & Modifier.STATIC) == Modifier.STATIC)
-            Class<?>[] params = method.getParameterTypes();
-            if (params.length == 1 && params[0] == int.class) {
-                System.out.println(method);
-                if (method.getName().equals("valueOf")) {
-                    String res=(String) method.invoke(null,123);
-                    System.out.println(res);
-                }
+            if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC) {
+                String str = method.toString();
+                String result = str.replace("java.lang.Math.", "");
+                System.out.println(result);
             }
-            //System.out.println(method);
+        }
+        Field[] fields = structMath.getFields();
+        for (Field field : fields) {
+            if ((field.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC) {
+                String str = field.toString();
+                String result = str.replace("java.lang.Math.", "");
+                System.out.println(result);
+            }
+
         }
     }
 }
