7cfe2f08bf84fdca3b2b75bb7854014371b43bc0
Pavel <pavel3423@gmail.com>
2018-01-12 13:27:33 +0300

    complite jd_10 C

--- src/by/it/kozlov/jd01_10/BeanTester.java
+++ src/by/it/kozlov/jd01_10/BeanTester.java
@@ -1,17 +1,18 @@
 package by.it.kozlov.jd01_10;
 
-import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class BeanTester {
-    public static void main(String[] args) throws IllegalAccessException, InstantiationException {
-        Class<?> bean=Bean.class;
-
-        Method[] methods=bean.getDeclaredMethods();
-        for (Method method:methods){
-            Annotation[] annotations=method.getAnnotations();
-            for (Annotation annotation:annotations)
-            System.out.println(annotation);
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, InvocationTargetException {
+        Class<?> structBean = Bean.class;
+        Object bObj = structBean.newInstance();
+        Method[] methods = structBean.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Param.class)) {
+                Param param = method.getAnnotation(Param.class);
+                System.out.println(method.getName()+method.invoke(bObj,param.a(),param.b()));
+            }
         }
     }
 }
