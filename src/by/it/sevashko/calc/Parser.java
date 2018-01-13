299b27d1d98b9cb4675ce04103ad3a08dbed1265
VladSVBY <vladsvby@yandex.by>
2018-01-13 18:39:23 +0300

    jd01_11 A and B completed

--- src/by/it/sevashko/calc/Parser.java
+++ src/by/it/sevashko/calc/Parser.java
@@ -6,15 +6,14 @@ import java.util.regex.Pattern;
 public class Parser {
 
     Var calc(String expression){
-        Pattern operationPattern=Pattern.compile(Patterns.OPERATION);
-        Matcher matcher=operationPattern.matcher(expression);
         String[] operand = expression.split(Patterns.OPERATION);
         Var one = Var.createVar(operand[0]);
         Var two = Var.createVar(operand[1]);
-
         if (one==null || two==null) {
             return null;
         }
+        Pattern operationPattern=Pattern.compile(Patterns.OPERATION);
+        Matcher matcher=operationPattern.matcher(expression);
         if (matcher.find()){
             String operation=matcher.group();
             switch (operation){
