afdf8bb3cde7464297a361e6246031a7e7985715
karpeichyk <pashkar@bk.ru>
2018-01-15 00:23:08 +0300

    01_09

--- src/by/it/karpeichyk/jd01_09/Operation.java
+++ src/by/it/karpeichyk/jd01_09/Operation.java
@@ -1,8 +1,11 @@
 package by.it.karpeichyk.jd01_09;
 
 /**
- * Created by user on 10.01.2018.
+ * Created by user on 13.01.2018.
  */
-public class Operation {
-
+public interface Operation {
+    Var add (Var other);
+    Var mul (Var other);
+    Var div (Var other);
+    Var sub (Var other);
 }
--- src/by/it/karpeichyk/jd01_09/Scalar.java
+++ src/by/it/karpeichyk/jd01_09/Scalar.java
@@ -6,5 +6,48 @@ package by.it.karpeichyk.jd01_09;
  * Created by user on 10.01.2018.
  */
  class Scalar extends Var {
+     private double value;
+
+    public Scalar(double value) {this.value=value;}
+
+
+    @Override
+     public Var add(Var other){
+         if(other instanceof Scalar){
+             return new Scalar(this.value+((Scalar) other).value);
+         }
+         else return other. add(this);
+     }
+     @Override
+    public Var mul (Var other){
+        if(other instanceof Scalar){
+            return new Scalar(this.value*((Scalar) other).value);
+        }
+        else return other. mul(this);
+    }
+    @Override
+    public Var div(Var other){
+        if(other instanceof Scalar){
+            return new Scalar(this.value/((Scalar) other).value);
+        }
+        else return other. div(this);
+    }
+    @Override
+    public Var sub(Var other){
+        if(other instanceof Scalar){
+            return new Scalar(this.value-((Scalar) other).value);
+        }
+        else{
+            Scalar minus=new Scalar(-1);
+            return other. sub(this).mul(minus);}
+    }
+    Scalar (Scalar other){this.value=other.value;}
+Scalar (String string){
+        this.value=Double.parseDouble(string);
+}
+    @Override
+    public  String toString(){return Double.toString(value);}
+
+
 
 }
