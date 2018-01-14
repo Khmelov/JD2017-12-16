f1457f82193662cfb2cca14dd4aa1df06fc5e9c8
VladSVBY <vladsvby@yandex.by>
2018-01-14 12:56:18 +0300

    jd01_11 A,B,C without calculator completed

--- src/by/it/sevashko/calc/Patterns.java
+++ src/by/it/sevashko/calc/Patterns.java
@@ -7,8 +7,9 @@ class Patterns {
     static final String SCALAR="((-?)([0-9.])+)";                       //числа
     static final String VECTOR="\\{((-?([0-9.])+),?)+}";                //вектора
     static final String MATRIX="\\{((\\{((-?([0-9.])+),?)+}),?)+}";     //матрицы
+    static final String NAME_VAR="";
 
     //этот паттерн весьма несовершенный, т.к. операции вида
     //3.0 * -4.1 работать не смогут. Проблему решим позднее.
-    static final String OPERATION="[-+*/]";                             //операция
+    static final String OPERATION="[-+*/=]";                             //операция
 }
