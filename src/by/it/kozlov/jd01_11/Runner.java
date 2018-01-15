e60ca2889e81d30b1e67b732053134a637c99c4a
Pavel <pavel3423@gmail.com>
2018-01-15 17:08:44 +0300

    complite jd_11 B

--- src/by/it/kozlov/jd01_11/Runner.java
+++ src/by/it/kozlov/jd01_11/Runner.java
@@ -4,13 +4,18 @@ import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
-        List<String> list=new ListA<>();
+        List<String> list=new ListB<>();
+        List<String> list2=new ListB<>();
         list.add("one");
-        list.add("one");
-        list.add("one");
-        list.add("one");
+        list.add("two");
+        list.add("three");
+        list.add("four");
+        list2.add("one");
+        list2.add("two");
+        list2.add("three");
+        list2.add("four");
         System.out.println(list);
-        list.remove(2);
+        list.addAll(list2);
         System.out.println(list);
     }
 }
