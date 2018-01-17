9a8238c67c1e3dab91232606e7b2062b5f11194d
Pavel <pavel3423@gmail.com>
2018-01-17 19:41:16 +0300

    jd01_12 B3

--- src/by/it/kozlov/jd01_12/TaskB2.java
+++ src/by/it/kozlov/jd01_12/TaskB2.java
@@ -6,36 +6,38 @@ import java.util.LinkedList;
 
 public class TaskB2 {
     public static void main(String[] args) {
-        int[] expectedRes = {1, 1, 3, 1, 3, 5, 7, 1, 3, 5, 7, 9, 11, 13, 15};
+        int[] expectedRes = {1, 1, 3, 1, 3, 5, 7, 1, 3, 5, 7, 9, 11, 13, 15, 1};
         ArrayList<String> arr = new ArrayList<>();
         LinkedList<String> link = new LinkedList<>();
-        for (int i = 1; i <= expectedRes.length; i++) {
-            arr.add("n" + i);
-            link.add("n" + i);
-        }
+            for (int i = 1; i <= expectedRes.length; i++) {
+                arr.add("n" + i);
+                link.add("n" + i);
+            }
         System.out.println(process(arr));
         System.out.println(process(link));
     }
 
     public static String process(ArrayList<String> peoples) {
-        boolean test = false;
+
         while (peoples.size() != 1) {
             Iterator<String> iterator = peoples.iterator();
+            boolean test = false;
             while (iterator.hasNext()) {
                 iterator.next();
                 if (test) {
                     iterator.remove();
                     test = false;
                 } else test = true;
+
             }
         }
         return peoples.get(0);
     }
 
     public static String process(LinkedList<String> peoples) {
-        boolean test = false;
         while (peoples.size() != 1) {
             Iterator<String> iterator = peoples.iterator();
+            boolean test = false;
             while (iterator.hasNext()) {
                 iterator.next();
                 if (test) {
@@ -46,4 +48,5 @@ public class TaskB2 {
         }
         return peoples.getFirst();
     }
+
 }
