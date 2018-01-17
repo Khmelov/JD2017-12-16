237a3c8678b8514eff10a30170385fa1555e4b7b
PatskoArkadzi <arkpatsko@gmail.com>
2018-01-17 22:44:50 +0300

    JD01_12 Tasks A, B, C1 completed

---
+++ src/by/it/patsko/jd01_12/TaskC2.java
@@ -0,0 +1,12 @@
+package by.it.patsko.jd01_12;
+/*
+TaskC2.
+Решить заново задачу TaskA2 для любого количества множеств на входе метода,
+а также для различных типов данных в каждом из множеств (generics).
+В main покажите работоспособность решения.
+ */
+public class TaskC2 {
+    public static void main(String[] args) {
+
+    }
+}
