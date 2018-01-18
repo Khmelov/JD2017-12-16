d0ff429f427c3f9584d0517105b3bc216240dd0e
Pavel <pavel3423@gmail.com>
2018-01-18 15:28:21 +0300

    jd01_13 A

--- src/by/it/kozlov/calc/Parser.java
+++ src/by/it/kozlov/calc/Parser.java
@@ -5,23 +5,27 @@ import java.util.regex.Pattern;
 
 public class Parser {
     Var calc(String expression) throws CalcException {
-        String[] operand = expression.split(Patterns.OPERATION);
-        Var one = Var.createVar(operand[0]);
-        Var two = Var.createVar(operand[1]);
-        if (one == null || two == null) return null;
-        Pattern operationPattern = Pattern.compile(Patterns.OPERATION);
-        Matcher matcher = operationPattern.matcher(expression);
-        if (matcher.find()) {
-            String operation = matcher.group();
-            switch (operation) {
-                case "+":
-                    return one.add(two);
-                case "-":
-                    return one.sub(two);
-                case "*":
-                    return one.mul(two);
-                case "/":
-                    return one.div(two);
+        // 2.0 * 2.0
+        String[] operand=expression.split(Patterns.OPERATION);
+        Var two=Var.createVar(operand[1]);
+        if (expression.contains("=")){
+            VarsMap.set(operand[0],two);
+            return two;
+        }
+
+        Var one=Var.createVar(operand[0]);
+        if (one==null || two==null)
+            throw new CalcException(" Ошибка парсинга выражения: "+expression);
+        //find "[-+*/]"
+        Pattern operationPattern=Pattern.compile(Patterns.OPERATION);
+        Matcher matcher=operationPattern.matcher(expression);
+        if (matcher.find()){
+            String operation=matcher.group();
+            switch (operation){
+                case "+": return one.add(two);
+                case "-": return one.sub(two);
+                case "*": return one.mul(two);
+                case "/": return one.div(two);
             }
         }
         return null;
