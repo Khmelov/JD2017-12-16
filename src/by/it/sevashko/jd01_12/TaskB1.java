9aa5f3399ab87b473454ba755dfa4bf74a218d86
VladSVBY <vladsvby@yandex.by>
2018-01-18 11:18:57 +0300

    jd01_12 A and B completed

--- src/by/it/sevashko/jd01_12/TaskB1.java
+++ src/by/it/sevashko/jd01_12/TaskB1.java
@@ -1,7 +1,6 @@
 package by.it.sevashko.jd01_12;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
--- src/by/it/sevashko/jd01_12/TaskC2.java
+++ src/by/it/sevashko/jd01_12/TaskC2.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class TaskA2 {
+public class TaskC2 {
 
     public static void main(String[] args) {
         HashSet<Integer> a = new HashSet<>(Arrays.asList(1,1,2,2,3,4,5,5));
@@ -18,15 +18,19 @@ public class TaskA2 {
         System.out.println(result);
     }
 
-    static Set<Integer> getCross(Set<Integer> a, Set<Integer> b){
+    static Set<Integer> getCross(Set<Integer> a, Set<Integer>... args){
         Set<Integer> result = new HashSet<>(a);
-        result.retainAll(b);
+        for (Set<Integer> set : args) {
+            result.retainAll(set);
+        }
         return result;
     }
 
-    static Set<Integer> getUnion(Set<Integer> a, Set<Integer> b){
-        Set<Integer> result = new HashSet<>(a);
-        result.addAll(b);
+    static <T> Set<T> getUnion(Set<T> a, Set<? extends T>... args){
+        Set<T> result = new HashSet<>(a);
+        for (Set<? extends T> set : args) {
+            result.addAll(set);
+        }
         return result;
     }
 }
