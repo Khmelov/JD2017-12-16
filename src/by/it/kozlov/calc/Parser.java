5321a400d78cb46820cbbf0bf52e1a290245e4ed
Pavel <pavel3423@gmail.com>
2018-01-18 15:12:21 +0300

    jd01_13 A

--- src/by/it/kozlov/calc/Parser.java
+++ src/by/it/kozlov/calc/Parser.java
@@ -3,20 +3,12 @@ package by.it.kozlov.calc;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-class Parser {
+public class Parser {
     Var calc(String expression) throws CalcException {
-        // 2.0 * 2.0
         String[] operand = expression.split(Patterns.OPERATION);
-        Var two = Var.createVar(operand[1]);
-        if (expression.contains("=")) {
-            VarsMap.set(operand[0], two);
-            return two;
-        }
-
         Var one = Var.createVar(operand[0]);
-        if (one == null || two == null)
-            throw new CalcException(String.format("Ошибка парсинга выражения: " + expression));
-        //find "[-+*/]"
+        Var two = Var.createVar(operand[1]);
+        if (one == null || two == null) return null;
         Pattern operationPattern = Pattern.compile(Patterns.OPERATION);
         Matcher matcher = operationPattern.matcher(expression);
         if (matcher.find()) {
