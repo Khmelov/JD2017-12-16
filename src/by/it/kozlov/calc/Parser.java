77b9dcc676ccd3102ee7436ff9f190d68eb710fc
Pavel <pavel3423@gmail.com>
2018-01-18 16:19:25 +0300

    jd01_13 A

--- src/by/it/kozlov/calc/Parser.java
+++ src/by/it/kozlov/calc/Parser.java
@@ -3,29 +3,33 @@ package by.it.kozlov.calc;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Parser {
+class Parser {
     Var calc(String expression) throws CalcException {
         // 2.0 * 2.0
-        String[] operand=expression.split(Patterns.OPERATION);
-        Var two=Var.createVar(operand[1]);
-        if (expression.contains("=")){
-            VarsMap.set(operand[0],two);
+        String[] operand = expression.split(Patterns.OPERATION);
+        Var two = Var.createVar(operand[1]);
+        if (expression.contains("=")) {
+            VarsMap.set(operand[0], two);
             return two;
         }
 
-        Var one=Var.createVar(operand[0]);
-        if (one==null || two==null)
-            throw new CalcException(" Ошибка парсинга выражения: "+expression);
+        Var one = Var.createVar(operand[0]);
+        if (one == null || two == null)
+            throw new CalcException(" Ошибка парсинга выражения: " + expression);
         //find "[-+*/]"
-        Pattern operationPattern=Pattern.compile(Patterns.OPERATION);
-        Matcher matcher=operationPattern.matcher(expression);
-        if (matcher.find()){
-            String operation=matcher.group();
-            switch (operation){
-                case "+": return one.add(two);
-                case "-": return one.sub(two);
-                case "*": return one.mul(two);
-                case "/": return one.div(two);
+        Pattern operationPattern = Pattern.compile(Patterns.OPERATION);
+        Matcher matcher = operationPattern.matcher(expression);
+        if (matcher.find()) {
+            String operation = matcher.group();
+            switch (operation) {
+                case "+":
+                    return one.add(two);
+                case "-":
+                    return one.sub(two);
+                case "*":
+                    return one.mul(two);
+                case "/":
+                    return one.div(two);
             }
         }
         return null;
