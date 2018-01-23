5ab9cc3c022dcdf9ab43a6a83cfcd1cf6b153a76
adultit <mrchnk.vld@mail.ru>
2018-01-23 12:13:46 +0300

    jd01_15 Classwork

---
+++ src/by/it/ikiselev/jd01_15/TaskA.java
@@ -0,0 +1,26 @@
+package by.it.ikiselev.jd01_15;
+
+import java.util.Random;
+
+public class TaskA {
+//    public static void main(String[] args) {
+//        int[][] m = new int[6][4];
+//        Random rnd= new Random();
+//        for (int i = 0; i < m.length; i++) {
+//            for (int j = 0; j < m[i].length; j++) {
+//                m[i][j]=-15+rnd.nextInt(31);
+//
+//            }
+//        }
+//
+//        for (int[] row:m) {
+//            for (int value:row) {
+//                System.out.printf("%3d",value);
+//
+//
+//            }
+//            System.out.println();
+//        }
+//
+//    }
+}
