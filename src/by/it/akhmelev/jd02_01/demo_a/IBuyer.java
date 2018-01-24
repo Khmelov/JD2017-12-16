4c203cbe3bc9ff2a9b082d4e2dd244f400280c43
Khmelov <375336849110@tut.by>
2018-01-25 00:23:42 +0300

    jd02_01 examlpes

---
+++ src/by/it/akhmelev/jd02_01/demo_a/IBuyer.java
@@ -0,0 +1,8 @@
+package by.it.akhmelev.jd02_01.demo_a;
+
+public interface IBuyer {
+    void enterToMarket();   //вошел в магазин
+    void chooseGoods();     //выбрал товары
+    void goToOut();         //отправился на выход
+}
+
---
+++ src/by/it/akhmelev/jd02_01/demo_a/IUseBasket.java
@@ -0,0 +1,7 @@
+package by.it.akhmelev.jd02_01.demo_a;
+
+public interface IUseBasket {
+    void takeBacket();      //взял корзину
+    void putGoodsToBacket();//положил выбранный товар в корзину
+}
+
