95e36f826991015dd24078b90cc5f82ee0e57be7
PatskoArkadzi <arkpatsko@gmail.com>
2018-01-24 20:22:09 +0300

    JD01_15 TaskA completed

--- src/by/it/patsko/jd01_15/TaskA.java
+++ src/by/it/patsko/jd01_15/TaskA.java
@@ -3,60 +3,95 @@ package by.it.patsko.jd01_15;
 import java.io.*;
 import java.util.Random;
 
+/*
+Класс TaskA
+Создайте матрицу 6 строк на 4 столбца из целых случайных чисел от -15 до 15 включительно.
+Выведите матрицу в текстовый файл matrix.txt, расположенный в папке задания jd01_15.
+При выводе для каждого числа нужно предусмотреть для него три знакоместа, после чисел –один пробел.
+Прочитайте файл и покажите его в консоли. Класс Scanner использовать нельзя.
+ */
 class TaskA {
-    public static void main(String[] args) {
-        int[][] matrix = generateMatrix();
-//        System.out.println(File.separator);
-        //output to txt file
-
-//                TaskA.class.getSimpleName()
-        saveMatrix((new File(getRoot(), "matrix.txt")), matrix);
-        showFile(new File(getRoot(), "matrix.txt"));
-        //read file
-    }
-    private static int[][] generateMatrix() {
-        Random rmd = new Random();
-        int[][] m = new int[6][4];
+    static int[][] generateMatrix(int cols, int rows) {
+        int[][] m = new int[cols][rows];
+        Random random = new Random();
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m[i].length; j++) {
-                m[i][j] = -15 + rmd.nextInt(31);
+                m[i][j] = random.nextInt(31) - 15;
+//                m[i][j]=(int)(Math.random()*31)-15;
             }
         }
         return m;
     }
 
-    private static String getRoot() {
-        return System.getProperty("user.dir") +
+    static void printMatrix(int[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                System.out.printf("%3d ", m[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    static String getPath() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(System.getProperty("user.dir"));
+        sb.append(File.separator);
+        sb.append("src");
+        sb.append(File.separator);
+        sb.append(TaskA.class.getName()
+                .replace(TaskA.class.getSimpleName(), "")
+                .replace(".", File.separator));
+        return sb.toString();
+        /*return System.getProperty("user.dir") +
                 File.separator + "src" + File.separator +
                 TaskA.class.getName().
                         replaceFirst(TaskA.class.getSimpleName(), "").
-                        replace(".", File.separator);
+                        replace(".", File.separator);*/
     }
 
-    private static void saveMatrix(File file, int[][] matrix) {
-        try (PrintWriter writer = new PrintWriter(
+    static void writeMatrix(File file, int[][] m) {
+        try (BufferedWriter writer = new BufferedWriter(
                 new FileWriter(file)
         )) {
-            for (int[] row : matrix) {
-                for (int value : row) {
-                    writer.printf("%3d ", value);
+            for (int i = 0; i < m.length; i++) {
+                for (int j = 0; j < m[i].length; j++) {
+                    writer.write(String.format("%3d ", m[i][j]));
                 }
-                System.out.println();
+                writer.newLine();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        /*try (PrintWriter writer=new PrintWriter(
+                new FileWriter(file)
+        )){
+            for (int i = 0; i < m.length; i++) {
+                for (int j = 0; j < m[i].length; j++) {
+                    writer.printf("%3d ", m[i][j]);
+                }
+                writer.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
     }
 
-    private static void showFile(File txtFile) {
+    static void readNShowMatrix(File file) {
         try (BufferedReader reader = new BufferedReader(
-                new FileReader(txtFile))
-        ) {
-            String line;
-            while ((line = reader.readLine()) != null)
-                System.out.println(line);
+                new FileReader(file)
+        )) {
+            String str;
+            while ((str = reader.readLine()) != null) System.out.println(str);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = generateMatrix(6, 4);
+        printMatrix(matrix);
+        System.out.println();
+        writeMatrix(new File(getPath(), "matrix.txt"), matrix);
+        readNShowMatrix(new File(getPath(), "matrix.txt"));
     }
 }
