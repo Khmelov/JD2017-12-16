b617552cc4097efac9296e2b90e60727f7a5681e
Pavel <pavel3423@gmail.com>
2018-01-26 09:36:37 +0300

    jd02_01 A

--- src/by/it/kozlov/jd02_01/Buyer.java
+++ src/by/it/kozlov/jd02_01/Buyer.java
@@ -1,7 +1,10 @@
 package by.it.kozlov.jd02_01;
 
-public class Buyer extends Thread implements IBuyer {
+class Buyer extends Thread implements IBuyer {
 
+    Buyer(int number) {
+        super("Покупатель №" + number);
+    }
 
     @Override
     public void run() {
@@ -12,16 +15,30 @@ public class Buyer extends Thread implements IBuyer {
 
     @Override
     public void enterToMarket() {
-
+        System.out.println(this + "зашел в магазин");
     }
 
     @Override
     public void chooseGoods() {
-
+        for (int i = 1; i <= Helper.getRandom(5); i++) {
+            Helper.sleep(500, 2000);
+            String goodName = Goods.rndGoodName();
+            Double goodPrice = Goods.getPrice(goodName);
+            System.out.println(this + "выбрал товар " + goodName + " цена:" + goodPrice);
+        }
+        System.out.println(this + "завершил выбор товаров");
     }
 
     @Override
     public void goToOut() {
+        //todo all is bad (sync)
+        Runner.queue.remove(this);
+        System.out.println(this + "вышел из магазина");
+        System.out.println("В очереди осталось " + Runner.queue.size());
+    }
 
+    @Override
+    public String toString() {
+        return this.getName() + " "; //"Покупатель №"+number
     }
 }
