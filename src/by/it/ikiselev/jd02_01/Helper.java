7fa1939a2567cefe3e914aaf37495b21960488c3
i_kiselyov <igorkiselev93@gmail.com>
2018-01-27 07:26:40 +0300

    jd02_01 A,B,C

---
+++ src/by/it/ikiselev/jd02_01/Helper.java
@@ -0,0 +1,24 @@
+package by.it.ikiselev.jd02_01;
+
+import java.util.Random;
+
+public class Helper {
+    private final static Random random=new Random();
+
+    static int getRandom(int bound){
+        return random.nextInt(bound);
+    }
+
+    private static int getRandom(int start, int stop){
+        return start+random.nextInt(stop-start);
+    }
+
+    static void sleep(int start, int stop){
+        try {
+
+            Thread.sleep(Helper.getRandom(start,stop));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
