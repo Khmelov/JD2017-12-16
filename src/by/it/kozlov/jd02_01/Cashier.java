7e29b9f0a4288165d0076e27c2967f5d0bbfc381
Pavel <pavel3423@gmail.com>
2018-01-29 23:06:30 +0300

    jd02_02 B-

--- src/by/it/kozlov/jd02_01/Cashier.java
+++ src/by/it/kozlov/jd02_01/Cashier.java
@@ -1,5 +1,8 @@
 package by.it.kozlov.jd02_01;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Cashier implements Runnable {
     private int number;
 
@@ -13,10 +16,24 @@ public class Cashier implements Runnable {
         while (!Dispetcher.allBuyerComplete()) {
             Buyer b = Dispetcher.extractFromQueue();
             if (b != null) {
+                HashMap<String, Double> check;
+                check = new HashMap<>(b.getGoods());
+
                 System.out.println(this + "Начало обслуживания " + b);
-                Helper.sleep(200, 500);
-                System.out.println(this + "печатаем чек для " + b);
-                System.out.println(this + "Конец обслуживания " + b);
+                Helper.sleep(2000, 5000);
+                if (check.size() > 0) {
+                    System.out.println(this + "печатаем чек для " + b);
+                    System.out.println(this + "в корзине у покупателя:");
+                    int sum = 0;
+                    for (Map.Entry<String, Double> entry : check.entrySet()) {
+                        System.out.println(entry.getKey() + " : " + entry.getValue());
+                        sum += entry.getValue();
+                    }
+                    System.out.println(this + b.toString() + "приобрёл товаров на сумму: " + sum + " Рублей");
+                } else {
+                    System.out.println(this + " Покупатель ничего не приобрёл");
+                }
+                System.out.println(this + " Конец обслуживания " + b);
                 synchronized (b) {
                     b.notify();
                 }
