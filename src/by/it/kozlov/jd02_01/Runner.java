9db868d829ff9c3665c9d6de9a7490fa380d6d28
Pavel <pavel3423@gmail.com>
2018-01-29 22:00:14 +0300

    jd02_02 A

--- src/by/it/kozlov/jd02_01/Runner.java
+++ src/by/it/kozlov/jd02_01/Runner.java
@@ -5,26 +5,26 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Runner: Магазин открыт");
         for (int i = 1; i <= 5; i++) {
-            Cashier c = new Cashier(i);
+            Cashier c=new Cashier(i);
             new Thread(c).start();
         }
 
-
-        int count = Helper.getRandom(5, 30);
-        for (int i = 0; i <= count; i++) {
-            Buyer b = new Buyer(Dispetcher.incCountBuyer());
-            b.start();
+        for (int second = 0; second < 12; second++) {
+            int count = Helper.getRandom(2);
+            for (int i = 0; i <= count; i++) {
+                Buyer b = new Buyer(Dispetcher.incCountBuyer());
+                b.start();
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
 
         while (!Dispetcher.allBuyerComplete()) {
-            Buyer first = Dispetcher.readFirstQueue();
-            if (first != null)
+            Buyer first=Dispetcher.readFirstQueue();
+            if (first!=null)
                 try {
                     first.join();
                 } catch (InterruptedException e) {
@@ -32,7 +32,7 @@ public class Runner {
                 }
         }
         System.out.println("Runner: Все вышли");
-        Helper.sleep(100, 200);
+        Helper.sleep(100,200);
         System.out.println("Runner: Магазин закрыт");
 
     }
