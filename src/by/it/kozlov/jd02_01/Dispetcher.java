345c68119a79936561c5ec63f314be41ce5652e9
Pavel <pavel3423@gmail.com>
2018-01-29 22:00:14 +0300

    jd02_02 A

--- src/by/it/kozlov/jd02_01/Dispetcher.java
+++ src/by/it/kozlov/jd02_01/Dispetcher.java
@@ -1,5 +1,49 @@
 package by.it.kozlov.jd02_01;
 
-public class Dispetcher {
+import java.util.LinkedList;
 
+public class Dispetcher {
+    private volatile static int countBuyer = 0;
+    private static int completeBuyer = 0;
+    private final static LinkedList<Buyer> queue = new LinkedList<>();
+
+    static int incCountBuyer() {
+        return ++countBuyer;
+    }
+
+    synchronized static boolean allBuyerComplete() {
+        return (countBuyer > 0) && (completeBuyer == countBuyer);
+    }
+
+    synchronized static int incCompleteBuyer() {
+        return ++completeBuyer;
+    }
+
+    synchronized static void printCounts() {
+        System.out.printf("\tDispatcher: Всего клиентов %d. Из них обслужено %d\n", countBuyer, completeBuyer);
+    }
+
+    static void addToQueue(Buyer b) {
+        synchronized (queue) {
+            queue.addLast(b);
+        }
+    }
+
+    static Buyer extractFromQueue() {
+        synchronized (queue) {
+            return queue.pollFirst();
+        }
+    }
+
+    static Buyer readFirstQueue() {
+        synchronized (queue) {
+            return queue.peekFirst();
+        }
+    }
+
+    static int getSizeQueue() {
+        synchronized (queue) {
+            return queue.size();
+        }
+    }
 }
