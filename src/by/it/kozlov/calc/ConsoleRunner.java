5038872775b7ed45a3274ee85e250cd673e01932
Pavel <pavel3423@gmail.com>
2018-02-02 20:01:37 +0300

    jd02_04 C

--- src/by/it/kozlov/calc/ConsoleRunner.java
+++ src/by/it/kozlov/calc/ConsoleRunner.java
@@ -4,21 +4,20 @@ import java.util.Scanner;
 
 public class ConsoleRunner {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         String line;
 
-        Parser parser=new Parser();
-        while (!(line=sc.nextLine()).equals("end")){
-            if (line.toLowerCase().contains("printvar"))
-            {
+        Parser parser = new Parser();
+        while (!(line = sc.nextLine()).equals("end")) {
+            if (line.toLowerCase().contains("printvar")) {
                 System.out.println(VarsMap.printvar());
                 continue;
             }
 
             try {
-                System.out.println(parser.calc(line.trim()));
+                System.out.println(parser.operations(line.trim()));
             } catch (CalcException e) {
-                System.out.println("ERROR: "+e.getMessage());
+                System.out.println("ERROR: " + e.getMessage());
             }
         }
     }
