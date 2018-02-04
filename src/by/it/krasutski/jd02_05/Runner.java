e49b0f79c1d0587450119f11187b68b446d2f781
George <georgekrasutski@gmail.com>
2018-02-05 01:32:45 +0300

    jd02_05 TaskA,B,C completed.

---
+++ src/by/it/krasutski/jd02_05/Runner.java
@@ -0,0 +1,46 @@
+package by.it.krasutski.jd02_05;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line;
+
+        while (!(line = scanner.nextLine()).equals("end"))
+            chooseLocale(line);
+    }
+
+    private static void chooseLocale(String command) {
+        ResMan manager = ResMan.INSTANCE;
+        Locale locale;
+        switch (command) {
+            case "ru":
+                locale = new Locale("ru", "RU");
+                manager.setLocale(locale);
+                printText(manager);
+                break;
+            case "be":
+                locale = new Locale("be", "BY");
+                manager.setLocale(locale);
+                printText(manager);
+                break;
+            case "en":
+                manager.setLocale(Locale.getDefault());
+                printText(manager);
+                break;
+            default:
+                System.out.println("Incorrect input. Please try again.");
+        }
+    }
+
+    private static void printText(ResMan manager) {
+        System.out.println(manager.getLocale(Msg.Welcome));
+        System.out.println(manager.getLocale(Msg.Name));
+        System.out.println(manager.getLocale(Msg.Question));
+        System.out.println(manager.currentData());
+        System.out.println("_________________");
+    }
+}
