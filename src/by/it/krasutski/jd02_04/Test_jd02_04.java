fa5cd05ff298b42dad1e5c6cfb294369bf1cf7fd
George <georgekrasutski@gmail.com>
2018-02-05 01:39:02 +0300

    test

--- src/by/it/krasutski/jd02_04/Test_jd02_04.java
+++ src/by/it/krasutski/jd02_04/Test_jd02_04.java
@@ -23,13 +23,15 @@ public class Test_jd02_04 {
         actual = parser.calc("B2=A/2-1");
         assertEquals("Error test", "2.65", actual);
 
-        //===================Scalar operations=========================
         actual = parser.calc("3.8+26.2");
         assertEquals("Error test", "30.0", actual);
+
         actual = parser.calc("87.4-23");
         assertEquals("Error test", "64.4", actual);
+
         actual = parser.calc("2.1*5.9");
         assertEquals("Error test", "12.39", actual);
+        
         actual = parser.calc("4/8");
         assertEquals("Error test", "0.5", actual);
 
@@ -48,13 +50,15 @@ public class Test_jd02_04 {
         actual = parser.calc("E={2,3}*(D/2)");
         assertEquals("Error test", "{10.0,15.0}", actual);
 
-        //===================Vector operations=========================
         actual = parser.calc("{2,3,4}*2");
         assertEquals("Error test", "{4.0,6.0,8.0}", actual);
+
         actual = parser.calc("{3,6,9}/3");
         assertEquals("Error test", "{1.0,2.0,3.0}", actual);
+
         actual = parser.calc("{2,3,4}-5");
         assertEquals("Error test", "{-3.0,-2.0,-1.0}", actual);
+
         actual = parser.calc("{2,3,4}+{5,6,7}");
         assertEquals("Error test", "{7.0,9.0,11.0}", actual);
     }
@@ -72,11 +76,12 @@ public class Test_jd02_04 {
         actual = parser.calc("E=(C+D)*3");
         assertEquals("Error test", "{{-7.5,1.5},{55.5,10.5}}", actual);
 
-        //===================Matrix operations=========================
         actual = parser.calc("{{1,2},{8,3}}-2");
         assertEquals("Error test", "{{-1.0,0.0},{6.0,1.0}}", actual);
+
         actual = parser.calc("{{1,2},{8,3}} * {{1,2},{8,3}}");
         assertEquals("Error test", "{{17.0,8.0},{32.0,25.0}}", actual);
+
         actual = parser.calc("{{1,2},{8,3}}+{{1,2},{8,3}}");
         assertEquals("Error test", "{{2.0,4.0},{16.0,6.0}}", actual);
     }
