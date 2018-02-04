e5ea0689d021a9f625de13ecdc147a6c34abad8b
PatskoArkadzi <arkpatsko@gmail.com>
2018-02-04 13:00:04 +0300

    jd02_05 TaskB,C completed

--- src/by/it/patsko/jd02_05/ResMan.java
+++ src/by/it/patsko/jd02_05/ResMan.java
@@ -1,22 +1,34 @@
 package by.it.patsko.jd02_05;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public enum ResMan {
     INSTANCE;
 
-    ResourceBundle rb;
-    Locale locale;
-    private final String path="by/it/patsko/jd02_05/resourse/str";
+    private ResourceBundle rb;
+    private Locale locale;
+    private final String path = "by/it/patsko/jd02_05/resource/str";
+    private DateFormat dateFormat;
+
     private ResMan() {
+        Locale.setDefault(new Locale("en", "US"));
         setLocale(Locale.getDefault());
+        dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM,Locale.getDefault());
     }
-    void setLocale(Locale locale){
-        this.rb = ResourceBundle.getBundle(path,locale);
+
+    void setLocale(Locale locale) {
+        this.rb = ResourceBundle.getBundle(path, locale);
         this.locale = locale;
+        dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM,locale);
     }
-    String get(String key){
+
+    String get(String key) {
         return rb.getString(key);
+    }
+    String currentData(){
+        return dateFormat.format(new Date());
     }
 }
--- src/by/it/patsko/jd02_05/resourse/convert.bat
+++
@@ -1,4 +0,0 @@
-cd .
-FOR %%I In (*.txt) DO (
-"C:\Program Files\Java\jdk1.8.0_144\bin\native2ascii.exe" -encoding utf-8 %%I %%~nI.properties
-)
