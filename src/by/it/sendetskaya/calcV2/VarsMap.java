83ccf888d739388e6c6fe7eb89e45cb5dd076dab
IrinaSendetskaya <Irene_siv@inbox.ru>
2018-02-05 13:19:47 +0300

    Task 02.04 completed

---
+++ src/by/it/sendetskaya/calcV2/VarsMap.java
@@ -0,0 +1,21 @@
+package by.it.sendetskaya.calcV2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class VarsMap {
+
+    private static Map<String,Var> map=new HashMap<>();
+
+    static Var get(String key){
+        return map.get(key.trim().toUpperCase());
+    };
+
+    static void set(String key, Var var){
+        map.put(key.trim().toUpperCase(), var);
+    }
+
+    static String printvar() {
+        return map.toString();
+    }
+}
