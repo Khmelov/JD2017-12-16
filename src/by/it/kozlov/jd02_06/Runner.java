7376fdf5e5d4aa01dcfe5c8032ae037395764f74
Pavel <pavel3423@gmail.com>
2018-02-07 15:13:36 +0300

    jd02_06 A

---
+++ src/by/it/kozlov/jd02_06/Runner.java
@@ -0,0 +1,8 @@
+package by.it.kozlov.jd02_06;
+
+public class Runner {
+    public static void main(String[] args) {
+        Instance instance=Instance.getInstance();
+        instance.logger("log1");
+    }
+}
