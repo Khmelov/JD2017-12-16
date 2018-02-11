950569e28fa0c346751ac506c7b0190229992943
Pavel <pavel3423@gmail.com>
2018-02-11 09:41:39 +0300

    jd02_07 C

--- src/by/it/kozlov/calc/VarFactory.java
+++ src/by/it/kozlov/calc/VarFactory.java
@@ -5,11 +5,11 @@ public class VarFactory {
         Var var = null;
         operand = operand.trim();
         if (operand.matches(Patterns.SCALAR))
-            return var = new Scalar(operand.replace(" ",""));
+            return var = new Scalar(operand);
         if (operand.matches(Patterns.VECTOR))
-            return var = new Vector(operand.replace(" ",""));
+            return var = new Vector(operand);
         if (operand.matches(Patterns.MATRIX))
-            return var = new Matrix(operand.replace(" ",""));
+            return var = new Matrix(operand);
         return VarsMap.get(operand);
     }
 }
