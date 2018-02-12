3b484503a685eb7559c628c23adb4771c773707f
VladSVBY <vladsvby@yandex.by>
2018-02-12 13:19:29 +0300

    jd02_06 A,B completed

---
+++ src/by/it/sevashko/jd02_06/Operation.java
@@ -0,0 +1,9 @@
+package by.it.sevashko.jd02_06;
+
+public interface Operation {
+
+    Var add(Var other) throws CalcException;
+    Var sub(Var other) throws CalcException;
+    Var mul(Var other) throws CalcException;
+    Var div(Var other) throws CalcException;
+}
