2f4ad39f379b39e4a427f6736e742efe8046acd0
Pavel <pavel3423@gmail.com>
2018-02-12 15:02:41 +0300

    jd02_08 B

--- src/by/it/kozlov/jd02_08/DOM.java
+++ src/by/it/kozlov/jd02_08/DOM.java
@@ -1,9 +1,6 @@
 package by.it.kozlov.jd02_08;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -12,6 +9,8 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
 public class DOM {
+    private static String buffer = "";
+
     public static void main(String[] args) {
         String path = "src/by/it/kozlov/jd02_07/Persons XSD.xml";
 
@@ -28,13 +27,36 @@ public class DOM {
 
     private static void printDOM(String prefix, Node node) {
         String text = node.getNodeValue();
+
+
         if (text != null) {
-            System.out.println(prefix + text.trim());
+            text = text.trim().replace("\n", "");
+            if (text.length() > 0) {
+                System.out.print(prefix + text.trim() + "\n");
+            }
+
         }
 
         NodeList children = node.getChildNodes();
+        if (children.getLength() > 0) {
+            System.out.print(prefix + "[" + node.getNodeName());
+            NamedNodeMap nodeMap = node.getAttributes();
+            for (int i = 0; i < nodeMap.getLength(); i++) {
+                System.out.print(" " + nodeMap.item(i));
+            }
+            System.out.println("]");
+            prefix = prefix + "\t";
+        }
+
         for (int i = 0; i < children.getLength(); i++) {
-            printDOM(prefix + node.getNodeName() + ">", children.item(i));
+
+            buffer = node.getNodeName();
+
+            printDOM(prefix, children.item(i));
+        }
+        if (children.getLength() > 0) {
+            prefix = prefix.substring(1);
+            System.out.println(prefix + "[/" + node.getNodeName() + "]");
         }
     }
 }
