cce5e79ce384394b79ee2a27499669fd9c20d832
karpeichyk <pashkar@bk.ru>
2018-02-13 00:39:36 +0300

    02_05 & 02_07

---
+++ src/by/it/karpeichyk/jd02_08/SaxParser.java
@@ -0,0 +1,16 @@
+package by.it.karpeichyk.jd02_08;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * Created by user on 10.02.2018.
+ */
+public class SaxParser extends DefaultHandler{
+    private String tab="";
+    private  StringBuilder text= new StringBuilder();
+
+    public static void main(String[] args) {
+
+    }
+
+}
