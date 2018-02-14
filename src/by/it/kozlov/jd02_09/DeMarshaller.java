3177e9b79bec8e692508aad7ae3d0613e3dddafa
Pavel <pavel3423@gmail.com>
2018-02-14 14:40:28 +0300

    jd02_09 C

---
+++ src/by/it/kozlov/jd02_09/DeMarshaller.java
@@ -0,0 +1,4 @@
+package by.it.kozlov.jd02_09;
+
+public class DeMarshaller {
+}
--- src/by/it/kozlov/jd02_09/ToXML.java
+++ src/by/it/kozlov/jd02_09/ToXML.java
@@ -1,25 +1,22 @@
-//--add-modules java.se.ee
-
 package by.it.kozlov.jd02_09;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-import java.io.*;
+import java.io.File;
 
 public class ToXML {
     public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(new File(System.getProperty("user.dir")+"/src/by/it/kozlov/jd02_09/Persons XSD.xml")))) {
+        try {
             JAXBContext context = JAXBContext.newInstance(Persons.class);
             Unmarshaller unmarshaller = context.createUnmarshaller();
-            Persons persons = (Persons) unmarshaller.unmarshal(reader);
-            String text = persons.toString();
-            System.out.println(text.replace("{", "\n").replace("}", "\n"));
+            Persons persons = (Persons) unmarshaller.unmarshal(new File("C:/Users/skynet/IdeaProjects/JD2017-12-16/src/by/it/kozlov/jd02_09/Persons2.xml"));
+            System.out.println(persons);
             Marshaller marshaller = context.createMarshaller();
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            marshaller.marshal(persons, new File(System.getProperty("user.dir")+"/src/by/it/kozlov/jd02_09/Persons2.xml"));
-        } catch (JAXBException | IOException e) {
+            marshaller.marshal(persons, new File("C:/Users/skynet/IdeaProjects/JD2017-12-16/src/by/it/kozlov/jd02_09/Persons3.xml"));
+        } catch (JAXBException e) {
             e.printStackTrace();
         }
     }
