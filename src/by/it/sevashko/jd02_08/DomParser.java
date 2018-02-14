f4701ce70aaca6d0a016bc6bdea74c96c7857775
VladSVBY <vladsvby@yandex.by>
2018-02-14 11:24:08 +0300

    jd02_08 C completed

--- src/by/it/sevashko/jd02_08/DomParser.java
+++ src/by/it/sevashko/jd02_08/DomParser.java
@@ -1,8 +1,63 @@
 package by.it.sevashko.jd02_08;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.Map;
+
 public class DomParser {
 
+    private static StringBuilder text = new StringBuilder();
+
     public static void main(String[] args) {
-        String xmlFile = "E:/Java/github/JD2017-12-16/src/by/it/sevashko/jd02_08/Books.xml";
+        String xmlFile = String.format("%s%S", System.getProperty("user.dir"), "/src/by/it/sevashko/jd02_08/Books.xml");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document xmlDocument = builder.parse(xmlFile);
+            Node rootElement = xmlDocument.getDocumentElement();
+            printNode("", rootElement);
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void printNode(String indent, Node node){
+        //начало тега и аттрибуты
+        System.out.printf("%s[%s", indent, node.getNodeName());
+        if (node.hasAttributes()) {
+            NamedNodeMap attrs = node.getAttributes();
+            for (int i = 0; i < attrs.getLength(); i++) {
+                Node attr = attrs.item(i);
+                    String name = attr.getNodeName();
+                    String value = attr.getTextContent();
+                    System.out.printf(" %s=%s", name, value);
+
+            }
+        }
+        System.out.println(']');
+        //String value = node.getTextContent();
+        //if (value != null) System.out.println(indent + value.trim());
+        //вывод вложееных тегов
+        if (node.hasChildNodes()) {
+            NodeList children = node.getChildNodes();
+            for (int i = 0; i < children.getLength(); i++) {
+                Node child = children.item(i);
+                if (child.getNodeType() == Node.ELEMENT_NODE) printNode(indent + '\t', child);
+                if (child.getNodeType() == Node.TEXT_NODE){
+                    String text = child.getTextContent().trim();
+                    if (!text.equals("")) System.out.printf("%s\t%s\n", indent, text);
+                }
+            }
+        }
+        System.out.printf("%s[/%s]\n", indent, node.getNodeName());
     }
 }
