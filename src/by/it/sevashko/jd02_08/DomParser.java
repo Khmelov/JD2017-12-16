5ca4c99a8e41eaebceb6f924c325e333af8a5438
VladSVBY <vladsvby@yandex.by>
2018-02-14 09:49:07 +0300

    jd02_08 B completed

---
+++ src/by/it/sevashko/jd02_08/DomParser.java
@@ -0,0 +1,8 @@
+package by.it.sevashko.jd02_08;
+
+public class DomParser {
+
+    public static void main(String[] args) {
+        String xmlFile = "E:/Java/github/JD2017-12-16/src/by/it/sevashko/jd02_08/Books.xml";
+    }
+}
