26ab54ee06dca70211c888bbd31100c22d9bc131
Pavel <pavel3423@gmail.com>
2018-02-16 14:26:00 +0300

    jd03_01 B

--- src/by/it/kozlov/jd03_01/A_AddUser.java
+++ src/by/it/kozlov/jd03_01/A_AddUser.java
@@ -14,9 +14,11 @@ public class A_AddUser {
             e.printStackTrace();
         }
 
-        try (Connection connection= (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:2016/kozlov?useUnicode=true&characterEncoding=UTF-8","root","")) {
-            Statement statement=connection.createStatement();
+        try (Connection connection = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:2016/kozlov?useUnicode=true&characterEncoding=UTF-8", "root", "")) {
+            Statement statement = connection.createStatement();
             statement.executeUpdate(String.format("INSERT INTO `kozlov`.`users` (`ID`, `Login`, `Email`, `Password`, `city_ID`, `Address`, `PhoneNumber`, `roles_ID`) VALUES (DEFAULT, 'bayernkraft.by', 'bmw.service@bayernkraft.by', 'bayernkraft', 7, 'ул. Панченко, 9', '+375447730077', 2);"));
+            statement.executeUpdate(String.format("INSERT INTO `kozlov`.`users` (`ID`, `Login`, `Email`, `Password`, `city_ID`, `Address`, `PhoneNumber`, `roles_ID`) VALUES (DEFAULT, 'mercedes-benz.by', 'info@mercedes-benz.by', 'mercedes', 7, 'ул. Тимирязева, 70', '+375296039999', 2);"));
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
