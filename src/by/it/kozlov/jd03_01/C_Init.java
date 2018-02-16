303e08a7024e915932058f13999a69a7f554da8a
Pavel <pavel3423@gmail.com>
2018-02-16 15:39:11 +0300

    jd03_01 C

--- src/by/it/kozlov/jd03_01/C_Init.java
+++ src/by/it/kozlov/jd03_01/C_Init.java
@@ -24,7 +24,7 @@ public class C_Init {
                     "  `brand` VARCHAR(50) NOT NULL,\n" +
                     "  PRIMARY KEY (`ID`))\n" +
                     "ENGINE = InnoDB\n" +
-                    "AUTO_INCREMENT = 1\n" +
+                    "AUTO_INCREMENT = 3\n" +
                     "DEFAULT CHARACTER SET = utf8;");
             statement.executeUpdate("DROP TABLE IF EXISTS `kozlov`.`city` ;");
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS `kozlov`.`city` (\n" +
@@ -32,7 +32,7 @@ public class C_Init {
                     "  `city` VARCHAR(45) NOT NULL,\n" +
                     "  PRIMARY KEY (`ID`))\n" +
                     "ENGINE = InnoDB\n" +
-                    "AUTO_INCREMENT = 1\n" +
+                    "AUTO_INCREMENT = 8\n" +
                     "DEFAULT CHARACTER SET = utf8;");
             statement.executeUpdate("DROP TABLE IF EXISTS `kozlov`.`roles` ;");
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS `kozlov`.`roles` (\n" +
@@ -40,7 +40,7 @@ public class C_Init {
                     "  `Role` VARCHAR(50) NOT NULL,\n" +
                     "  PRIMARY KEY (`ID`))\n" +
                     "ENGINE = InnoDB\n" +
-                    "AUTO_INCREMENT = 1\n" +
+                    "AUTO_INCREMENT = 3\n" +
                     "DEFAULT CHARACTER SET = utf8;");
             statement.executeUpdate("DROP TABLE IF EXISTS `kozlov`.`users` ;");
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS `kozlov`.`users` (\n" +
@@ -84,29 +84,28 @@ public class C_Init {
                     "    ON DELETE CASCADE\n" +
                     "    ON UPDATE CASCADE)\n" +
                     "ENGINE = InnoDB\n" +
-                    "AUTO_INCREMENT = 1\n" +
+                    "AUTO_INCREMENT = 5\n" +
                     "DEFAULT CHARACTER SET = utf8;");
-            statement.executeUpdate("INSERT INTO `kozlov`.`brand` (`brand`) VALUES ('BMW'); ");
-            statement.executeUpdate("INSERT INTO `kozlov`.`brand` (`brand`) VALUES ('Mercedes-Benz');");
+            statement.executeUpdate("INSERT INTO `kozlov`.`brand` (`ID`, `brand`) VALUES (1, 'BMW'); ");
+            statement.executeUpdate("INSERT INTO `kozlov`.`brand` (`ID`, `brand`) VALUES (2, 'Mercedes-Benz');");
 
-            statement.executeUpdate("INSERT INTO `kozlov`.`city` (`city`) VALUES ('Брестская область');");
-            statement.executeUpdate("INSERT INTO `kozlov`.`city` (`city`) VALUES ('Витебская область');");
-            statement.executeUpdate("INSERT INTO `kozlov`.`city` (`city`) VALUES ('Гомельская область');");
-            statement.executeUpdate("INSERT INTO `kozlov`.`city` (`city`) VALUES ('Гродненская область');");
-            statement.executeUpdate("INSERT INTO `kozlov`.`city` (`city`) VALUES ('Минская область');");
-            statement.executeUpdate("INSERT INTO `kozlov`.`city` (`city`) VALUES ('Могилевская область');");
-            statement.executeUpdate("INSERT INTO `kozlov`.`city` (`city`) VALUES ('г. Минск');");
+            statement.executeUpdate("INSERT INTO `kozlov`.`city` (`ID`, `city`) VALUES (1, 'Брестская область');");
+            statement.executeUpdate("INSERT INTO `kozlov`.`city` (`ID`, `city`) VALUES (2, 'Витебская область');");
+            statement.executeUpdate("INSERT INTO `kozlov`.`city` (`ID`, `city`) VALUES (3, 'Гомельская область');");
+            statement.executeUpdate("INSERT INTO `kozlov`.`city` (`ID`, `city`) VALUES (4, 'Гродненская область');");
+            statement.executeUpdate("INSERT INTO `kozlov`.`city` (`ID`, `city`) VALUES (5, 'Минская область');");
+            statement.executeUpdate("INSERT INTO `kozlov`.`city` (`ID`, `city`) VALUES (6, 'Могилевская область');");
+            statement.executeUpdate("INSERT INTO `kozlov`.`city` (`ID`, `city`) VALUES (7, 'г. Минск');");
 
-            statement.executeUpdate("INSERT INTO `kozlov`.`roles` (`Role`) VALUES ('admin');");
-            statement.executeUpdate("INSERT INTO `kozlov`.`roles` (`Role`) VALUES ('user');");
+            statement.executeUpdate("INSERT INTO `kozlov`.`roles` (`ID`, `Role`) VALUES (1, 'admin');");
+            statement.executeUpdate("INSERT INTO `kozlov`.`roles` (`ID`, `Role`) VALUES (2, 'user');");
 
-            statement.executeUpdate("INSERT INTO `kozlov`.`users` (`Login`, `Email`, `Password`, `city_ID`, `Address`, `PhoneNumber`, `roles_ID`) VALUES ('bayernkraft.by', 'bmw.service@bayernkraft.by', 'bayernkraft', 7, 'ул. Панченко, 9', '+375447730077', 2);");
-            statement.executeUpdate("INSERT INTO `kozlov`.`users` (`Login`, `Email`, `Password`, `city_ID`, `Address`, `PhoneNumber`, `roles_ID`) VALUES ('mercedes-benz.by', 'info@mercedes-benz.by', 'mercedes', 7, 'ул. Тимирязева, 70', '+375296039999', 2);");
-
-            statement.executeUpdate("INSERT INTO `kozlov`.`cars` (`brand_ID`, `Model`, `Class`, `Price`, `Year`, `users_ID`) VALUES (1, '7 series', 'Седан', 164400, 2018, 1);\n");
-            statement.executeUpdate("INSERT INTO `kozlov`.`cars` (`brand_ID`, `Model`, `Class`, `Price`, `Year`, `users_ID`) VALUES (1, 'X6', 'Кроссовер', 132000, 2018, 1);");
-            statement.executeUpdate("INSERT INTO `kozlov`.`cars` (`brand_ID`, `Model`, `Class`, `Price`, `Year`, `users_ID`) VALUES (2, 'E 200 4MATIC', 'Седан', 137706, 2018, 2);");
-            statement.executeUpdate("INSERT INTO `kozlov`.`cars` (`brand_ID`, `Model`, `Class`, `Price`, `Year`, `users_ID`) VALUES (2, 'GLS', 'Внедорожник', 164700, 2018, 2);");
+            statement.executeUpdate("INSERT INTO `kozlov`.`users` (`ID`, `Login`, `Email`, `Password`, `city_ID`, `Address`, `PhoneNumber`, `roles_ID`) VALUES (1, 'bayernkraft.by', 'bmw.service@bayernkraft.by', 'bayernkraft', 7, 'ул. Панченко, 9', '+375447730077', 2);");
+            statement.executeUpdate("INSERT INTO `kozlov`.`users` (`ID`, `Login`, `Email`, `Password`, `city_ID`, `Address`, `PhoneNumber`, `roles_ID`) VALUES (2, 'mercedes-benz.by', 'info@mercedes-benz.by', 'mercedes', 7, 'ул. Тимирязева, 70', '+375296039999', 2);");
+            statement.executeUpdate("INSERT INTO `kozlov`.`cars` (`ID`, `brand_ID`, `Model`, `Class`, `Price`, `Year`, `users_ID`) VALUES (1, 1, '7 series', 'Седан', 164400, 2018, 1);\n");
+            statement.executeUpdate("INSERT INTO `kozlov`.`cars` (`ID`, `brand_ID`, `Model`, `Class`, `Price`, `Year`, `users_ID`) VALUES (2, 1, 'X6', 'Кроссовер', 132000, 2018, 1);");
+            statement.executeUpdate("INSERT INTO `kozlov`.`cars` (`ID`, `brand_ID`, `Model`, `Class`, `Price`, `Year`, `users_ID`) VALUES (3, 2, 'E 200 4MATIC', 'Седан', 137706, 2018, 2);");
+            statement.executeUpdate("INSERT INTO `kozlov`.`cars` (`ID`, `brand_ID`, `Model`, `Class`, `Price`, `Year`, `users_ID`) VALUES (4, 2, 'GLS', 'Внедорожник', 164700, 2018, 2);");
 
         } catch (SQLException e) {
             e.printStackTrace();
