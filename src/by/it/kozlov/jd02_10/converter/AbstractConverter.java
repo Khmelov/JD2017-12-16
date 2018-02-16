2374be3459987119a2aa71900a0c3a1ee6e1ac7e
Pavel <pavel3423@gmail.com>
2018-02-16 21:10:31 +0300

    jd02_10 B

---
+++ src/by/it/kozlov/jd02_10/converter/AbstractConverter.java
@@ -0,0 +1,6 @@
+package by.it.kozlov.jd02_10.converter;
+
+public abstract class AbstractConverter<T> implements IConverter {
+    public <T> void convert(T type) {
+    }
+}
---
+++ src/by/it/kozlov/jd02_10/converter/Runner.java
@@ -0,0 +1,31 @@
+//--add-modules java.se.ee
+
+package by.it.kozlov.jd02_10.converter;
+
+import by.it.kozlov.jd02_10.Persons;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Runner {
+    public static void main(String[] args) {
+        JAXBContext context = null;
+        try {
+            context = JAXBContext.newInstance(Persons.class);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(System.getProperty("user.dir") + "/src/by/it/kozlov/jd02_09/Persons XSD.xml")))) {
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            Persons persons = (Persons) unmarshaller.unmarshal(reader);
+            AbstractConverter converter = ConverterFactory.convert(1);
+        } catch (IOException | JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+}
