e797edad8a59d9d45e06323891bf1640615213f8
Pavel <pavel3423@gmail.com>
2018-02-16 14:26:00 +0300

    jd03_01 B

---
+++ src/by/it/kozlov/jd03_01/B_ShowUsers.java
@@ -0,0 +1,24 @@
+package by.it.kozlov.jd03_01;
+
+import java.sql.*;
+
+public class B_ShowUsers {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try (Connection connection = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:2016/kozlov?useUnicode=true&characterEncoding=UTF-8", "root", "")) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet=statement.executeQuery("select * from users");
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("Login")+", "+resultSet.getString("roles_ID"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
