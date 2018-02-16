1c290a59861255b37d9e61e2f6fb3a3772c197e8
Pavel <pavel3423@gmail.com>
2018-02-16 15:39:11 +0300

    jd03_01 C

--- src/by/it/kozlov/jd03_01/C_Reset.java
+++ src/by/it/kozlov/jd03_01/C_Reset.java
@@ -2,7 +2,7 @@ package by.it.kozlov.jd03_01;
 
 import java.sql.*;
 
-public class B_ShowUsers {
+public class C_Reset {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -12,10 +12,11 @@ public class B_ShowUsers {
 
         try (Connection connection = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:2016/kozlov?useUnicode=true&characterEncoding=UTF-8", "root", "")) {
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from users");
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("Login") + ", " + resultSet.getString("roles_ID"));
-            }
+            statement.executeUpdate("DELETE FROM `cars` WHERE 1");
+            statement.executeUpdate("DELETE FROM `users` WHERE 1");
+            statement.executeUpdate("DELETE FROM `brand` WHERE 1");
+            statement.executeUpdate("DELETE FROM `city` WHERE 1");
+            statement.executeUpdate("DELETE FROM `roles` WHERE 1");
 
         } catch (SQLException e) {
             e.printStackTrace();
