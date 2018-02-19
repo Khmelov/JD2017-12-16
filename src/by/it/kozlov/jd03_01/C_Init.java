faedd9963eed2a499150d312bfb827123d2b4fc6
skynet <pavel3423@gmail.com>
2018-02-19 19:32:52 +0300

    jd03_02 A

--- src/by/it/kozlov/jd03_01/C_Init.java
+++ src/by/it/kozlov/jd03_01/C_Init.java
@@ -20,67 +20,67 @@ public class C_Init {
             statement.executeUpdate("USE `kozlov` ;");
             statement.executeUpdate("DROP TABLE IF EXISTS `kozlov`.`brand` ;");
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS `kozlov`.`brand` (\n" +
-                    "  `ID` INT(11) NOT NULL AUTO_INCREMENT,\n" +
+                    "  `id` INT(11) NOT NULL AUTO_INCREMENT,\n" +
                     "  `brand` VARCHAR(50) NOT NULL,\n" +
-                    "  PRIMARY KEY (`ID`))\n" +
+                    "  PRIMARY KEY (`id`))\n" +
                     "ENGINE = InnoDB\n" +
                     "AUTO_INCREMENT = 1\n" +
                     "DEFAULT CHARACTER SET = utf8;");
             statement.executeUpdate("DROP TABLE IF EXISTS `kozlov`.`city` ;");
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS `kozlov`.`city` (\n" +
-                    "  `ID` INT(11) NOT NULL AUTO_INCREMENT,\n" +
+                    "  `id` INT(11) NOT NULL AUTO_INCREMENT,\n" +
                     "  `city` VARCHAR(45) NOT NULL,\n" +
-                    "  PRIMARY KEY (`ID`))\n" +
+                    "  PRIMARY KEY (`id`))\n" +
                     "ENGINE = InnoDB\n" +
                     "AUTO_INCREMENT = 1\n" +
                     "DEFAULT CHARACTER SET = utf8;");
             statement.executeUpdate("DROP TABLE IF EXISTS `kozlov`.`roles` ;");
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS `kozlov`.`roles` (\n" +
-                    "  `ID` INT(11) NOT NULL AUTO_INCREMENT,\n" +
-                    "  `Role` VARCHAR(50) NOT NULL,\n" +
-                    "  PRIMARY KEY (`ID`))\n" +
+                    "  `id` INT(11) NOT NULL AUTO_INCREMENT,\n" +
+                    "  `role` VARCHAR(50) NOT NULL,\n" +
+                    "  PRIMARY KEY (`id`))\n" +
                     "ENGINE = InnoDB\n" +
                     "AUTO_INCREMENT = 1\n" +
                     "DEFAULT CHARACTER SET = utf8;");
             statement.executeUpdate("DROP TABLE IF EXISTS `kozlov`.`users` ;");
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS `kozlov`.`users` (\n" +
-                    "  `ID` INT(11) NOT NULL AUTO_INCREMENT,\n" +
-                    "  `Login` VARCHAR(50) NOT NULL,\n" +
-                    "  `Email` VARCHAR(50) NOT NULL,\n" +
-                    "  `Password` VARCHAR(50) NOT NULL,\n" +
-                    "  `city_ID` INT(11) NOT NULL,\n" +
-                    "  `Address` VARCHAR(100) NOT NULL,\n" +
-                    "  `PhoneNumber` VARCHAR(50) NOT NULL,\n" +
-                    "  `roles_ID` INT(11) NOT NULL,\n" +
+                    "  `id` INT(11) NOT NULL AUTO_INCREMENT,\n" +
+                    "  `login` VARCHAR(50) NOT NULL,\n" +
+                    "  `email` VARCHAR(50) NOT NULL,\n" +
+                    "  `password` VARCHAR(50) NOT NULL,\n" +
+                    "  `cityID` INT(11) NOT NULL,\n" +
+                    "  `address` VARCHAR(100) NOT NULL,\n" +
+                    "  `phoneNumber` VARCHAR(50) NOT NULL,\n" +
+                    "  `rolesID` INT(11) NOT NULL,\n" +
                     "  PRIMARY KEY (`ID`),\n" +
-                    "  INDEX `fk_users_roles_idx` (`roles_ID` ASC),\n" +
-                    "  INDEX `fk_users_city1_idx` (`city_ID` ASC),\n" +
+                    "  INDEX `fk_users_roles_idx` (`rolesID` ASC),\n" +
+                    "  INDEX `fk_users_city1_idx` (`cityID` ASC),\n" +
                     "  CONSTRAINT `fk_users_city1`\n" +
-                    "    FOREIGN KEY (`city_ID`)\n" +
-                    "    REFERENCES `kozlov`.`city` (`ID`),\n" +
+                    "    FOREIGN KEY (`cityID`)\n" +
+                    "    REFERENCES `kozlov`.`city` (`id`),\n" +
                     "  CONSTRAINT `fk_users_roles`\n" +
-                    "    FOREIGN KEY (`roles_ID`)\n" +
-                    "    REFERENCES `kozlov`.`roles` (`ID`))\n" +
+                    "    FOREIGN KEY (`rolesID`)\n" +
+                    "    REFERENCES `kozlov`.`roles` (`id`))\n" +
                     "ENGINE = InnoDB\n" +
                     "DEFAULT CHARACTER SET = utf8;\n");
             statement.executeUpdate("DROP TABLE IF EXISTS `kozlov`.`cars` ;");
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS `kozlov`.`cars` (\n" +
-                    "  `ID` INT(11) NOT NULL AUTO_INCREMENT,\n" +
-                    "  `brand_ID` INT(11) NOT NULL,\n" +
-                    "  `Model` VARCHAR(50) NOT NULL,\n" +
-                    "  `Class` VARCHAR(50) NOT NULL,\n" +
-                    "  `Price` DOUBLE NOT NULL,\n" +
-                    "  `Year` INT(11) NOT NULL,\n" +
-                    "  `users_ID` INT(11) NOT NULL,\n" +
-                    "  PRIMARY KEY (`ID`),\n" +
-                    "  INDEX `fk_cars_users1_idx` (`users_ID` ASC),\n" +
-                    "  INDEX `fk_cars_brand1_idx` (`brand_ID` ASC),\n" +
+                    "  `id` INT(11) NOT NULL AUTO_INCREMENT,\n" +
+                    "  `brandID` INT(11) NOT NULL,\n" +
+                    "  `model` VARCHAR(50) NOT NULL,\n" +
+                    "  `carClass` VARCHAR(50) NOT NULL,\n" +
+                    "  `price` DOUBLE NOT NULL,\n" +
+                    "  `year` INT(11) NOT NULL,\n" +
+                    "  `usersID` INT(11) NOT NULL,\n" +
+                    "  PRIMARY KEY (`id`),\n" +
+                    "  INDEX `fk_cars_users1_idx` (`usersID` ASC),\n" +
+                    "  INDEX `fk_cars_brand1_idx` (`brandID` ASC),\n" +
                     "  CONSTRAINT `fk_cars_brand1`\n" +
-                    "    FOREIGN KEY (`brand_ID`)\n" +
-                    "    REFERENCES `kozlov`.`brand` (`ID`),\n" +
+                    "    FOREIGN KEY (`brandID`)\n" +
+                    "    REFERENCES `kozlov`.`brand` (`id`),\n" +
                     "  CONSTRAINT `fk_cars_users1`\n" +
-                    "    FOREIGN KEY (`users_ID`)\n" +
-                    "    REFERENCES `kozlov`.`users` (`ID`)\n" +
+                    "    FOREIGN KEY (`usersID`)\n" +
+                    "    REFERENCES `kozlov`.`users` (`id`)\n" +
                     "    ON DELETE CASCADE\n" +
                     "    ON UPDATE CASCADE)\n" +
                     "ENGINE = InnoDB\n" +
@@ -97,16 +97,16 @@ public class C_Init {
             statement.executeUpdate("INSERT INTO `kozlov`.`city` (`city`) VALUES ('Могилевская область');");
             statement.executeUpdate("INSERT INTO `kozlov`.`city` (`city`) VALUES ('г. Минск');");
 
-            statement.executeUpdate("INSERT INTO `kozlov`.`roles` (`Role`) VALUES ('admin');");
-            statement.executeUpdate("INSERT INTO `kozlov`.`roles` (`Role`) VALUES ('user');");
+            statement.executeUpdate("INSERT INTO `kozlov`.`roles` (`role`) VALUES ('admin');");
+            statement.executeUpdate("INSERT INTO `kozlov`.`roles` (`role`) VALUES ('user');");
 
-            statement.executeUpdate("INSERT INTO `kozlov`.`users` (`Login`, `Email`, `Password`, `city_ID`, `Address`, `PhoneNumber`, `roles_ID`) VALUES ('bayernkraft.by', 'bmw.service@bayernkraft.by', 'bayernkraft', 7, 'ул. Панченко, 9', '+375447730077', 2);");
-            statement.executeUpdate("INSERT INTO `kozlov`.`users` (`Login`, `Email`, `Password`, `city_ID`, `Address`, `PhoneNumber`, `roles_ID`) VALUES ('mercedes-benz.by', 'info@mercedes-benz.by', 'mercedes', 7, 'ул. Тимирязева, 70', '+375296039999', 2);");
+            statement.executeUpdate("INSERT INTO `kozlov`.`users` (`login`, `email`, `password`, `cityID`, `address`, `phoneNumber`, `rolesID`) VALUES ('bayernkraft.by', 'bmw.service@bayernkraft.by', 'bayernkraft', 7, 'ул. Панченко, 9', '+375447730077', 2);");
+            statement.executeUpdate("INSERT INTO `kozlov`.`users` (`login`, `email`, `password`, `cityID`, `address`, `phoneNumber`, `rolesID`) VALUES ('mercedes-benz.by', 'info@mercedes-benz.by', 'mercedes', 7, 'ул. Тимирязева, 70', '+375296039999', 2);");
 
-            statement.executeUpdate("INSERT INTO `kozlov`.`cars` (`brand_ID`, `Model`, `Class`, `Price`, `Year`, `users_ID`) VALUES (1, '7 series', 'Седан', 164400, 2018, 1);\n");
-            statement.executeUpdate("INSERT INTO `kozlov`.`cars` (`brand_ID`, `Model`, `Class`, `Price`, `Year`, `users_ID`) VALUES (1, 'X6', 'Кроссовер', 132000, 2018, 1);");
-            statement.executeUpdate("INSERT INTO `kozlov`.`cars` (`brand_ID`, `Model`, `Class`, `Price`, `Year`, `users_ID`) VALUES (2, 'E 200 4MATIC', 'Седан', 137706, 2018, 2);");
-            statement.executeUpdate("INSERT INTO `kozlov`.`cars` (`brand_ID`, `Model`, `Class`, `Price`, `Year`, `users_ID`) VALUES (2, 'GLS', 'Внедорожник', 164700, 2018, 2);");
+            statement.executeUpdate("INSERT INTO `kozlov`.`cars` (`brandID`, `model`, `carClass`, `price`, `year`, `usersID`) VALUES (1, '7 series', 'Седан', 164400, 2018, 1);\n");
+            statement.executeUpdate("INSERT INTO `kozlov`.`cars` (`brandID`, `model`, `carClass`, `price`, `year`, `usersID`) VALUES (1, 'X6', 'Кроссовер', 132000, 2018, 1);");
+            statement.executeUpdate("INSERT INTO `kozlov`.`cars` (`brandID`, `model`, `carClass`, `price`, `year`, `usersID`) VALUES (2, 'E 200 4MATIC', 'Седан', 137706, 2018, 2);");
+            statement.executeUpdate("INSERT INTO `kozlov`.`cars` (`brandID`, `model`, `carClass`, `price`, `year`, `usersID`) VALUES (2, 'GLS', 'Внедорожник', 164700, 2018, 2);");
 
         } catch (SQLException e) {
             e.printStackTrace();
