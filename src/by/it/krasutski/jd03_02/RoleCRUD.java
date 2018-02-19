66c9ea1fca61f27b480f8fd0fbd382774276b344
George <georgekrasutski@gmail.com>
2018-02-19 19:38:02 +0300

    jd03_02 TaskA,B completed.

---
+++ src/by/it/krasutski/jd03_02/RoleCRUD.java
@@ -0,0 +1,40 @@
+package by.it.krasutski.jd03_02;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class RoleCRUD implements BeanCRUD<Role> {
+
+    @Override
+    public boolean create(Role bean) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Role bean) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public Role read(int ID) throws SQLException {
+        try (Connection connection = ConnectionCreator.getConnection();
+             Statement statement = connection.createStatement()
+        ){
+            String query = String.format("SELECT * FROM `roles` WHERE ID=%d;", ID);
+            ResultSet result = statement.executeQuery(query);
+            if (result.next()){
+                return new Role(
+                        result.getInt("ID"),
+                        result.getString("Role"));
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean delete(Role bean) throws SQLException {
+        return false;
+    }
+}
