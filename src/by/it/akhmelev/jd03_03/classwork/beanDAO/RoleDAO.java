32cdc1b6c9f00e8c63ab30f09a644a347ecb676f
Khmelov <37533684911@tut.by>
2018-02-20 12:19:59 +0300

    03_03 classwork

---
+++ src/by/it/akhmelev/jd03_03/classwork/beanDAO/RoleDAO.java
@@ -0,0 +1,52 @@
+package by.it.akhmelev.jd03_03.classwork.beanDAO;
+
+
+import by.it.akhmelev.jd03_02.classwork.ConnectionCreator;
+import by.it.akhmelev.jd03_03.classwork.bean.Role;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author i.sukach
+ */
+public class RoleDAO implements InterfaceDAO<Role>{
+    @Override
+    public boolean create(Role bean) throws SQLException {
+        throw new SQLException("Not implements");
+    }
+
+    @Override
+    public boolean update(Role bean) throws SQLException {
+        throw new SQLException("Not implements");
+    }
+
+    @Override
+    public boolean delete(Role bean) throws SQLException {
+        throw new SQLException("Not implements");
+    }
+
+    @Override
+    public Role read(int id) throws SQLException {
+        List<Role> list=getAll(" where ID="+id);
+        return list.size()>0?list.get(0):null;
+    }
+
+    @Override
+    public List<Role> getAll(String where) throws SQLException {
+        List<Role> list=new ArrayList<>();
+        Connection connection= ConnectionCreator.getConnection();
+        Statement statement=connection.createStatement();
+        String sql="SELECT * from roles "+where+";";
+        ResultSet rs=statement.executeQuery(sql);
+        while (rs.next()){
+            Role role=new Role(rs.getInt("ID"),rs.getString("Role"));
+            list.add(role);
+        }
+        return list;
+    }
+}
