e995ce6565721214b22de893379595f60b2dbac5
skynet <pavel3423@gmail.com>
2018-02-22 17:30:42 +0300

    jd03_04 A

---
+++ src/by/it/kozlov/project/java/ComandError.java
@@ -0,0 +1,10 @@
+package by.it.kozlov.project.java;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ComandError implements ActionComand{
+    @Override
+    public String execute(HttpServletRequest req) {
+        return null;
+    }
+}
