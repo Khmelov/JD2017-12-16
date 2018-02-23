08828f785ac8e7c98d675f07f14a103221dc74b1
VladSVBY <vladsvby@yandex.by>
2018-02-23 17:33:22 +0300

    jd02_09

---
+++ src/by/it/sevashko/project/java/CommandLogin.java
@@ -0,0 +1,11 @@
+package by.it.sevashko.project.java;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class CommandLogin implements ActionCommand {
+
+    @Override
+    public String execute(HttpServletRequest req) {
+        return null;
+    }
+}
