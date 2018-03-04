06f5e4b3eb055fbfd8189d4c104829d6706ea90c
PatskoArkadzi <arkpatsko@gmail.com>
2018-03-04 17:38:58 +0300

    jd03_06 classwork

--- src/by/it/patsko/project/java/FormUtil.java
+++ src/by/it/patsko/project/java/FormUtil.java
@@ -1,18 +1,13 @@
 package by.it.patsko.project.java;
 
 import javax.servlet.http.HttpServletRequest;
-import java.text.ParseException;
 
 public class FormUtil {
-    /*public static String getString(HttpServletRequest request, String parametr, String pattern) throws ParseException {
-        String value=request.getParameter(parametr);
-        if((value!=null)&&value.matches(pattern)) return value;
-        else{
-            throw new ParseException("Input error: "+parametr+"\nrequest.getParameter(parametr)="+request.getParameter(parametr)+"\n\n", 1);}
-    }*/
-    public static String getString(String value, String regxp) throws ParseException {
-        if (value.matches(regxp)) return value;
-        else throw new ParseException("Input error", 1);
+
+    public static String getString(String value, String regxp) {
+        if(value==null) return null;
+        else if (value.matches(regxp)) return value;
+        else return null;
     }
 
     public static boolean isPost(HttpServletRequest reg) {
--- src/by/it/patsko/project/java/commands/CommandLogIn.java
+++ src/by/it/patsko/project/java/commands/CommandLogIn.java
@@ -20,18 +20,18 @@ public class CommandLogIn implements ActionCommand {
         }
         if (!FormUtil.isPost(req)) return Actions.LOGIN.jsp;
         try {
-
-
             String testLogin = FormUtil.getString(req.getParameter("Login"), Pattern.LOGIN);
-            String testPassword = FormUtil.getString(req.getParameter("Password"), Pattern.LOGIN);
+            String testPassword = FormUtil.getString(req.getParameter("Password"), Pattern.PASSWORD);
             Buyer buyer = new BuyerDAO().read(new BuyerDAO().read(testLogin, testPassword));
             LogInBuyer.getInstance(buyer);
 
             req.setAttribute(Msg.PROFILE_LOGIN, LogInBuyer.currentBuyer.getLogin());
+            req.setAttribute(Msg.PROFILE_PASSWORD, LogInBuyer.currentBuyer.getPassword());
+
 //            req.setAttribute(Msg.BUYER, LogInBuyer.currentBuyer.getLogin());
 
             req.setAttribute(Msg.MESSAGE, "Залогинился пользователь" + LogInBuyer.currentBuyer);
-        } catch (ParseException | NullPointerException e) {
+        } catch (NullPointerException e) {
             req.setAttribute(Msg.ERROR, "Такого пользователя не существует");
             req.setAttribute(Msg.ERROR_DETAILS, "<h5>details:</h5>" + Arrays.toString(e.getStackTrace()));
             return Actions.ERROR.jsp;
