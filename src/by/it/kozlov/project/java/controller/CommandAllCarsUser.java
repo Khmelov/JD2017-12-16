17578b70773292bd0bb8f729b5c49612ba1cf3da
skynet <pavel3423@gmail.com>
2018-03-09 19:51:03 +0300

    jd03_07 add edit cars user

--- src/by/it/kozlov/project/java/controller/CommandAllCarsUser.java
+++ src/by/it/kozlov/project/java/controller/CommandAllCarsUser.java
@@ -16,21 +16,61 @@ import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.List;
 
 public class CommandAllCarsUser extends Action {
     @Override
-    Action execute(HttpServletRequest request, HttpServletResponse response) throws SQLException, NoSuchPaddingException, NoSuchAlgorithmException, IOException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException {
+    Action execute(HttpServletRequest request, HttpServletResponse response) throws SQLException {
         HttpSession session = request.getSession();
-        User user = (User)session.getAttribute("user");
-            if (user == null) {
-                request.setAttribute(Message.MESSAGE, "Войдите чтобы просмотреть объявления");
-                return Actions.LOGIN.command;
-            }
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            request.setAttribute(Message.MESSAGE, "Войдите чтобы просмотреть объявления");
+            return Actions.LOGIN.command;
+        }
         List<Car> cars = DAO.getDAO().car.getAll(String.format("WHERE usersID=%d", user.getId()));
         request.setAttribute("cars", cars);
         List<Brand> brands = DAO.getDAO().brand.getAll();
         request.setAttribute("brands", brands);
-        return null;
+        if (FormUtil.isPost(request)) {
+            Car car;
+            if (request.getParameter("Delete") != null) {
+                car = new Car();
+                car.setId(Integer.parseInt(request.getParameter("Delete")));
+                DAO.getDAO().car.delete(car);
+                request.setAttribute(Message.MESSAGE, "Автомобиль удалён");
+                cars = DAO.getDAO().car.getAll(String.format("WHERE usersID=%d", user.getId()));
+                request.setAttribute("cars", cars);
+                return null;
+            } else {
+                try {
+                    if (request.getParameter("Price") != "") {
+                        car = new Car(0,
+                                Integer.parseInt(request.getParameter("Brand")),
+                                FormUtil.getString(request.getParameter("Model"), "[A-Za-z0-9_А-Яа-яЁё]+"),
+                                FormUtil.getString(request.getParameter("CarClass"), "[A-Za-z0-9_А-Яа-яЁё]+"),
+                                Double.parseDouble(request.getParameter("Price")),
+                                Integer.parseInt(request.getParameter("Year")),
+                                user.getId()
+                        );
+                    } else {
+                        request.setAttribute(Message.MESSAGE, "Введите цену");
+                        return null;
+                    }
+                } catch (ParseException e) {
+                    request.setAttribute(Message.MESSAGE, "Введены недопустимые символы");
+                    return null;
+                }
+                car.setId(Integer.parseInt(request.getParameter("Submit")));
+                DAO.getDAO().car.update(car);
+                request.setAttribute(Message.MESSAGE, "Данные изменены");
+                cars = DAO.getDAO().car.getAll(String.format("WHERE usersID=%d", user.getId()));
+                request.setAttribute("cars", cars);
+                return null;
+            }
+        } else {
+            return null;
+        }
     }
 }
+
--- src/by/it/kozlov/project/webapp/login.jsp
+++ src/by/it/kozlov/project/webapp/login.jsp
@@ -1,5 +1,6 @@
 <%@page language="java" pageEncoding="utf-8" %>
 <%@ include file="include/begin-html.jsp" %>
+
 <p>${message}</p>
 <form class="form-horizontal" action="do?command=Login" method="POST">
     <fieldset>
@@ -13,7 +14,7 @@
         <div class="form-group">
             <label class="col-md-4 control-label" for="Login">Login</label>
             <div class="col-md-4">
-                <input id="Login" name="Login" type="text" placeholder="Login" class="form-control input-md">
+                <input id="Login" name="Login" type="text" placeholder="Login" class="form-control input-md" value="bayernkraft.by">
 
             </div>
         </div>
@@ -22,7 +23,7 @@
         <div class="form-group">
             <label class="col-md-4 control-label" for="Password">Password</label>
             <div class="col-md-4">
-                <input id="Password" name="Password" type="password" placeholder="Password" class="form-control input-md">
+                <input id="Password" name="Password" type="password" placeholder="Password" class="form-control input-md" value="bayernkraft">
             </div>
         </div>
 
