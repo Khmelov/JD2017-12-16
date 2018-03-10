69b9b2aa31c4a009a9178316693fb7b341061c35
George <georgekrasutski@gmail.com>
2018-03-10 14:08:56 +0300

    jd03_07 fixes.

--- src/by/it/kozlov/project/java/controller/FrontController.java
+++ src/by/it/kozlov/project/java/controller/FrontController.java
@@ -1,28 +1,20 @@
 package by.it.kozlov.project.java.controller;
 
-import by.it.kozlov.project.java.filters.CookiesUser;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.text.ParseException;
 
 public class FrontController extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            process(request, response);
+            process(req, resp);
         } catch (ParseException | SQLException e) {
             e.printStackTrace();
         }
@@ -30,43 +22,36 @@ public class FrontController extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            process(request, response);
+            process(req, resp);
         } catch (ParseException | SQLException e) {
             e.printStackTrace();
         }
     }
 
-    private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ParseException, SQLException {
+    private void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, ParseException, SQLException {
         ActionFactory actionFactory = new ActionFactory();
-        Action command = actionFactory.defineCommand(request);
+        Action command = actionFactory.defineCommand(req);
         Action nextStep = null;
         ServletContext servletContext = getServletContext();
-        HttpSession session = request.getSession();
-        if (session.getAttribute(Message.MESSAGE) != null) {
-            request.setAttribute(Message.MESSAGE, session.getAttribute(Message.MESSAGE));
-            session.setAttribute(Message.MESSAGE, null);
-        }
         try {
-            CookiesUser.getSession(request);
-        } catch (NoSuchPaddingException | BadPaddingException | NoSuchAlgorithmException | IllegalBlockSizeException | InvalidKeyException e) {
-            e.printStackTrace();
-        }
-        try {
-            nextStep = command.execute(request, response);
+            nextStep = command.execute(req, resp);
         } catch (Exception e) {
-            request.setAttribute(Message.ERROR, e.getMessage());
+            req.setAttribute(Message.ERROR, e.getMessage());
             String errorJsp = Actions.ERROR.command.getJsp();
             RequestDispatcher dispatcher = servletContext.getRequestDispatcher(errorJsp);
-            dispatcher.forward(request, response);
+            dispatcher.forward(req, resp);
         }
         if (nextStep == null || nextStep == command) {
             String viewJsp = command.getJsp();
             RequestDispatcher dispatcher = servletContext.getRequestDispatcher(viewJsp);
-            dispatcher.forward(request, response);
+            dispatcher.forward(req, resp);
         } else {
-            response.sendRedirect("do?command=" + nextStep);
+            String viewJsp = nextStep.getJsp();
+            RequestDispatcher dispatcher = servletContext.getRequestDispatcher(viewJsp);
+            dispatcher.forward(req, resp);
+            resp.sendRedirect("do?command=" + nextStep);
         }
     }
 }
